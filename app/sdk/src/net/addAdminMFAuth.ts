03a066c1ea0cc37567edc0936628e3e62757b573
vuong <vuongdo_777@yahoo.com>
2025-01-01 15:13:24 +0000

    Add styles - improving security

--- app/sdk/src/net/addAdminMFAuth.ts
+++ app/sdk/src/net/addAdminMFAuth.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addAdminMFAuth(server: string, secure: boolean, token: string): Promise<{ secretImage: string, secretText: string }> {
-  const endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}`;
-  const mfa = await fetchWithTimeout(endpoint, { method: 'POST' });
+  let endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}`;
+  let mfa = await fetchWithTimeout(endpoint, { method: 'POST' });
   checkResponse(mfa.status);
   return await mfa.json();
 }
