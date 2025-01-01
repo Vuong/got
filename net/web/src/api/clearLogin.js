cd62c97f8593bdb4a76f0e21ec208f2e052012a4
vuong <vuongdo_777@yahoo.com>
2025-01-01 23:03:22 +0000

    Revert configuration - refactoring code

--- net/web/src/api/clearLogin.js
+++ net/web/src/api/clearLogin.js
@@ -3,8 +3,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 export async function clearLogin(token, all) {
 console.log("LOGOUT: ", token, all);
 
-  const param = all ? '&all=true' : ''
-  const logout = await fetchWithTimeout(`/account/apps?agent=${token}${param}`, { method: 'DELETE' })
+  let param = all ? '&all=true' : ''
+  let logout = await fetchWithTimeout(`/account/apps?agent=${token}${param}`, { method: 'DELETE' })
   checkResponse(logout)
 }
 
