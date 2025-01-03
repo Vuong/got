2337a1ba7c5bf5d63778275a3448b218913afa7c
vuong <vuongdo_777@yahoo.com>
2025-01-03 08:41:34 +0000

    Document UI - refactoring code

--- app/mobile/src/api/setCardCloseMessage.js
+++ app/mobile/src/api/setCardCloseMessage.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setCardCloseMessage(server, message) {
-  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  const protocol = insecure ? 'http' : 'https';
+  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  var protocol = insecure ? 'http' : 'https';
 
   let status = await fetchWithTimeout(`${protocol}://${server}/contact/closeMessage`, { method: 'PUT', body: JSON.stringify(message) });
   checkResponse(status);
