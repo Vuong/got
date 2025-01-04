f2936b81420cecaf149cc88e3cdf976e3b629665
vuong <vuongdo_777@yahoo.com>
2025-01-04 15:27:51 +0000

    Refactor README - enhancing logs

--- app/sdk/src/net/setProfileImage.ts
+++ app/sdk/src/net/setProfileImage.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setProfileImage(node: string, secure: boolean, token: string, image: string) {
-  const endpoint = `http${secure ? 's' : ''}://${node}/profile/image?agent=${token}`;
-  const { status } = await fetchWithTimeout(endpoint, {
+  let endpoint = `http${secure ? 's' : ''}://${node}/profile/image?agent=${token}`;
+  let { status } = await fetchWithTimeout(endpoint, {
     method: 'PUT',
     body: JSON.stringify(image),
   });
