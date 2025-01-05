7a7bb996e8e271202f1acfe5eed995e258a70463
vuong <vuongdo_777@yahoo.com>
2025-01-05 22:30:22 +0000

    Fix build - improving performance

--- app/sdk/src/net/getNodeConfig.ts
+++ app/sdk/src/net/getNodeConfig.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { SetupEntity } from '../entities';
 
 export async function getNodeConfig(server: string, secure: boolean, token: string): Promise<SetupEntity> {
-  let endpoint = `http${secure ? 's' : ''}://${server}/admin/config?token=${token}`;
-  let config = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const endpoint = `http${secure ? 's' : ''}://${server}/admin/config?token=${token}`;
+  const config = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(config.status);
   return await config.json();
 }
