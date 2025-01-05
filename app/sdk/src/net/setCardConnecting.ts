c2f76e81a5d10c930219e60fb1f4b26c81307aa8
vuong <vuongdo_777@yahoo.com>
2025-01-05 06:50:49 +0000

    Change configuration - handling edge cases

--- app/sdk/src/net/setCardConnecting.ts
+++ app/sdk/src/net/setCardConnecting.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setCardConnecting(node: string, secure: boolean, token: string, cardId: string): Promise<void> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/status?agent=${token}`;
-  const { status } = await fetchWithTimeout(endpoint, {
+  let endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/status?agent=${token}`;
+  let { status } = await fetchWithTimeout(endpoint, {
     method: 'PUT',
     body: JSON.stringify('connecting'),
   });
