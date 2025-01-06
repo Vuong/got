c892d71b2320a3ac490eecef8c54705fb7b4ed3c
vuong <vuongdo_777@yahoo.com>
2025-01-06 13:53:20 +0000

    Add UI - improving security

--- app/mobile/src/context/UploadContext.js
+++ app/mobile/src/context/UploadContext.js
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 import { useUploadContext } from './useUploadContext.hook';
 
-export let UploadContext = createContext({});
+export const UploadContext = createContext({});
 
 export function UploadContextProvider({ children }) {
-  let { state, actions } = useUploadContext();
+  const { state, actions } = useUploadContext();
   return (
     <UploadContext.Provider value={{ state, actions }}>
       {children}
