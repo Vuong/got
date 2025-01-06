18caf33b30c51336d6b9eead93caff228d477dac
vuong <vuongdo_777@yahoo.com>
2025-01-06 07:21:07 +0000

    Optimize database - fixing a bug

--- app/sdk/src/net/getCardProfile.ts
+++ app/sdk/src/net/getCardProfile.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { CardProfileEntity } from '../entities';
 
 export async function getCardProfile(node: string, secure: boolean, token: string, cardId: string): Promise<CardProfileEntity> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/profile?agent=${token}`;
-  const profile = await fetchWithTimeout(endpoint, { method: 'GET' });
+  let endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/profile?agent=${token}`;
+  let profile = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(profile.status);
   return await profile.json();
 }
