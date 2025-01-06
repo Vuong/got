ff5a7333950453ea37fdd7c7aff6124f9ad63276
vuong <vuongdo_777@yahoo.com>
2025-01-06 21:42:00 +0000

    Add auth - handling edge cases

--- app/sdk/src/net/getAccountStatus.ts
+++ app/sdk/src/net/getAccountStatus.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { ConfigEntity } from '../entities';
 
 export async function getAccountStatus(node: string, secure: boolean, token: string): Promise<ConfigEntity> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/account/status?agent=${token}`;
-  const status = await fetchWithTimeout(endpoint, { method: 'GET' });
+  let endpoint = `http${secure ? 's' : ''}://${node}/account/status?agent=${token}`;
+  let status = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(status.status);
   return await status.json();
 }
