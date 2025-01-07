ed0b8feb94b298355ee76c3fcb3f992977c155fe
vuong <vuongdo_777@yahoo.com>
2025-01-07 03:09:12 +0000

    Add API - updating docs

--- net/web/src/session/cardSelect/CardSelect.styled.js
+++ net/web/src/session/cardSelect/CardSelect.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Colors } from 'constants/Colors';
 
-export const CardSelectWrapper = styled.div`
+export let CardSelectWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
