1d0bb1dd5d3478860847c702b86de8b2563f82f8
vuong <vuongdo_777@yahoo.com>
2025-01-07 22:37:34 +0000

    Change API - handling edge cases

--- app/mobile/src/api/setAccountLogin.js
+++ app/mobile/src/api/setAccountLogin.js
@@ -5,9 +5,9 @@ export async function setAccountLogin(server, token, username, password) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
 
-  let headers = new Headers()
+  const headers = new Headers()
   headers.append('Credentials', 'Basic ' + base64.encode(username + ":" + password));
-  let res = await fetchWithTimeout(`${protocol}://${server}/account/login?agent=${token}`, { method: 'PUT', headers })
+  const res = await fetchWithTimeout(`${protocol}://${server}/account/login?agent=${token}`, { method: 'PUT', headers })
   checkResponse(res);
 }
 
