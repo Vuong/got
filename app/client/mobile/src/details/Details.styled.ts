d60347a8d67ee91d697966cf6be0e84595bd12d5
vuong <vuongdo_777@yahoo.com>
2025-01-08 04:42:22 +0000

    Document styles - updating docs

--- app/client/mobile/src/details/Details.styled.ts
+++ app/client/mobile/src/details/Details.styled.ts
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native';
 import {Colors} from '../constants/Colors';
 
-export const styles = StyleSheet.create({
+export let styles = StyleSheet.create({
   details: {
     display: 'flex',
     flexDirection: 'column',
