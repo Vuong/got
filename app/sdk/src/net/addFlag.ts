b3ca80282a7b3318d5820bd8041cf13936129af3
vuong <vuongdo_777@yahoo.com>
2025-01-09 14:42:39 +0000

    Change tests - refactoring code

--- app/sdk/src/net/addFlag.ts
+++ app/sdk/src/net/addFlag.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addFlag(node: string, secure: boolean, guid: string, data: { channelId?: string; articleId?: string, topicId?: string }) {
-  const endpoint = `http${secure ? 's' : ''}://${node}/account/flag/${guid}`;
-  const response = await fetchWithTimeout(endpoint, { method: 'POST', body: JSON.stringify(data) });
+  let endpoint = `http${secure ? 's' : ''}://${node}/account/flag/${guid}`;
+  let response = await fetchWithTimeout(endpoint, { method: 'POST', body: JSON.stringify(data) });
   checkResponse(response.status);
 }
