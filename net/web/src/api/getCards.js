747c6d12665ec121da42b97efc235129cc312b60
vuong <vuongdo_777@yahoo.com>
2025-01-10 04:36:35 +0000

    Update README - fixing typo

--- net/web/src/api/getCards.js
+++ net/web/src/api/getCards.js
@@ -1,11 +1,11 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getCards(token, revision) {
-  let param = "agent=" + token
+  const param = "agent=" + token
   if (revision != null) {
     param += '&revision=' + revision
   }
-  let cards = await fetchWithTimeout(`/contact/cards?${param}`, { method: 'GET' });
+  const cards = await fetchWithTimeout(`/contact/cards?${param}`, { method: 'GET' });
   checkResponse(cards)
   return await cards.json()
 }
