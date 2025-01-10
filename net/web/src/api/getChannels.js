39cbc7e4457e3e4d9febd67321db6981e7ed0d2b
vuong <vuongdo_777@yahoo.com>
2025-01-10 20:43:08 +0000

    Change styles - improving security

--- net/web/src/api/getChannels.js
+++ net/web/src/api/getChannels.js
@@ -1,15 +1,15 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getChannels(token, revision) {
-  let param = "?agent=" + token
+  const param = "?agent=" + token
   if (revision != null) {
     param += `&channelRevision=${revision}`
   }
-  let types = encodeURIComponent(JSON.stringify([ 'sealed', 'superbasic' ]));
+  const types = encodeURIComponent(JSON.stringify([ 'sealed', 'superbasic' ]));
   param += `&types=${types}`
-  let channels = await fetchWithTimeout('/content/channels' + param, { method: 'GET' });
+  const channels = await fetchWithTimeout('/content/channels' + param, { method: 'GET' });
   checkResponse(channels)
-  let ret = await channels.json()
+  const ret = await channels.json()
   return ret;
 }
 
