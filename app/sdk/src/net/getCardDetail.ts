a40caa60d9932f542174daa2dbd1e061ba26b4b3
vuong <vuongdo_777@yahoo.com>
2025-01-14 05:43:46 +0000

    Revert database - adding new feature

--- app/sdk/src/net/getCardDetail.ts
+++ app/sdk/src/net/getCardDetail.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { CardDetailEntity } from '../entities';
 
 export async function getCardDetail(node: string, secure: boolean, token: string, cardId: string): Promise<CardDetailEntity> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/detail?agent=${token}`;
-  let detail = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/detail?agent=${token}`;
+  const detail = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(detail.status);
   return await detail.json();
 }
