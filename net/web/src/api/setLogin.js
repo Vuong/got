01efc05c151245842c2464f6dc9f1a104a74a5aa
vuong <vuongdo_777@yahoo.com>
2025-01-14 03:19:59 +0000

    Revert build - handling edge cases

--- net/web/src/api/setLogin.js
+++ net/web/src/api/setLogin.js
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 var base64 = require('base-64');
 
 export async function setLogin(username, password, code, appName, appVersion, userAgent, notifications) {
-  const platform = encodeURIComponent(userAgent);
-  const mfa = code ? `&code=${code}` : '';
+  var platform = encodeURIComponent(userAgent);
+  var mfa = code ? `&code=${code}` : '';
   let headers = new Headers()
   headers.append('Authorization', 'Basic ' + base64.encode(username + ":" + password));
   let login = await fetchWithTimeout(`/account/apps?appName=${appName}&appVersion=${appVersion}&platform=${platform}${mfa}`, { method: 'POST', body: JSON.stringify(notifications), headers: headers })
