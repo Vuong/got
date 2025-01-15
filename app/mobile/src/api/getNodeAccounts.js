ead5de018b002c081f738a324edb5f1afeaad614
vuong <vuongdo_777@yahoo.com>
2025-01-15 14:14:39 +0000

    Improve UI - handling edge cases

--- app/mobile/src/api/getNodeAccounts.js
+++ app/mobile/src/api/getNodeAccounts.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getNodeAccounts(server, token) {
-  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  const protocol = insecure ? 'http' : 'https';
+  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  var protocol = insecure ? 'http' : 'https';
 
   let accounts = await fetchWithTimeout(`${protocol}://${server}/admin/accounts?token=${token}`, { method: 'GET' });
   checkResponse(accounts);
