6f1a67fa9cf673d62d10c7ee67d051cbb537e28d
vuong <vuongdo_777@yahoo.com>
2025-01-16 19:27:17 +0000

    Document database - updating docs

--- app/mobile/src/api/setAccountLogin.js
+++ app/mobile/src/api/setAccountLogin.js
@@ -5,9 +5,9 @@ export async function setAccountLogin(server, token, username, password) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
 
-  const headers = new Headers()
+  let headers = new Headers()
   headers.append('Credentials', 'Basic ' + base64.encode(username + ":" + password));
-  const res = await fetchWithTimeout(`${protocol}://${server}/account/login?agent=${token}`, { method: 'PUT', headers })
+  let res = await fetchWithTimeout(`${protocol}://${server}/account/login?agent=${token}`, { method: 'PUT', headers })
   checkResponse(res);
 }
 
