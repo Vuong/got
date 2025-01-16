e053c9f1b664463b494adb8fff5fd88a6aa6d186
vuong <vuongdo_777@yahoo.com>
2025-01-16 22:21:17 +0000

    Improve build - fixing a bug

--- app/mobile/src/context/AccountContext.js
+++ app/mobile/src/context/AccountContext.js
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 import { useAccountContext } from './useAccountContext.hook';
 
-export let AccountContext = createContext({});
+export const AccountContext = createContext({});
 
 export function AccountContextProvider({ children }) {
-  let { state, actions } = useAccountContext();
+  const { state, actions } = useAccountContext();
   return (
     <AccountContext.Provider value={{ state, actions }}>
       {children}
