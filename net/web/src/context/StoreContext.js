df14b19201a44ee108f3c4e1099245efbe7c5a03
vuong <vuongdo_777@yahoo.com>
2025-01-17 08:12:20 +0000

    Add auth - updating docs

--- net/web/src/context/StoreContext.js
+++ net/web/src/context/StoreContext.js
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 import { useStoreContext } from './useStoreContext.hook';
 
-export let StoreContext = createContext({});
+export const StoreContext = createContext({});
 
 export function StoreContextProvider({ children }) {
-  let { state, actions } = useStoreContext();
+  const { state, actions } = useStoreContext();
   return (
     <StoreContext.Provider value={{ state, actions }}>
       {children}
