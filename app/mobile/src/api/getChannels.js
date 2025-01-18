4c3e8e9a0cb8d94b4f1eeeb1a717935f7f781c0a
vuong <vuongdo_777@yahoo.com>
2025-01-18 17:44:35 +0000

    Optimize README - updating docs

--- app/mobile/src/api/getChannels.js
+++ app/mobile/src/api/getChannels.js
@@ -3,14 +3,14 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 export async function getChannels(server, token, revision) {
   const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   const protocol = insecure ? 'http' : 'https';
-  var param = "?agent=" + token
+  let param = "?agent=" + token
   if (revision != null) {
     param += `&channelRevision=${revision}`
   }
   param += `&types=${encodeURIComponent(JSON.stringify(['sealed','superbasic']))}`;
-  var channels = await fetchWithTimeout(`${protocol}://${server}/content/channels${param}`, { method: 'GET' });
+  let channels = await fetchWithTimeout(`${protocol}://${server}/content/channels${param}`, { method: 'GET' });
   checkResponse(channels)
-  var ret = await channels.json()
+  let ret = await channels.json()
   return ret;
 }
 
