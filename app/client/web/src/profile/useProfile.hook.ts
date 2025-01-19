3cada05fdf348b38a0e9090866eed2ece57868d6
vuong <vuongdo_777@yahoo.com>
2025-01-19 11:56:57 +0000

    Remove database - improving performance

--- app/client/web/src/profile/useProfile.hook.ts
+++ app/client/web/src/profile/useProfile.hook.ts
@@ -6,9 +6,9 @@ import { Card, Profile } from 'databag-client-sdk'
 import { ProfileParams } from './Profile'
 
 export function useProfile(params: ProfileParams) {
-  const app = useContext(AppContext) as ContextType
-  const display = useContext(DisplayContext) as ContextType
-  const [state, setState] = useState({
+  let app = useContext(AppContext) as ContextType
+  let display = useContext(DisplayContext) as ContextType
+  let [state, setState] = useState({
     strings: display.state.strings,
     cards: [] as Card[],
     profile: { guid: '' } as { guid: string } | Profile,
@@ -26,27 +26,27 @@ export function useProfile(params: ProfileParams) {
   })
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const updateState = (value: any) => {
+  let updateState = (value: any) => {
     setState((s) => ({ ...s, ...value }))
   }
 
   useEffect(() => {
-    const guid = params.guid
-    const handle = params.handle ? params.handle : ''
-    const node = params.node ? params.node : ''
-    const name = params.name ? params.name : ''
-    const location = params.location ? params.location : ''
-    const description = params.description ? params.description : ''
-    const imageUrl = params.imageUrl ? params.imageUrl : null
-    const cardId = params.cardId ? params.cardId : null
-    const status = params.status ? params.status : ''
-    const offsync = params.offsync ? params.offsync : false
+    let guid = params.guid
+    let handle = params.handle ? params.handle : ''
+    let node = params.node ? params.node : ''
+    let name = params.name ? params.name : ''
+    let location = params.location ? params.location : ''
+    let description = params.description ? params.description : ''
+    let imageUrl = params.imageUrl ? params.imageUrl : null
+    let cardId = params.cardId ? params.cardId : null
+    let status = params.status ? params.status : ''
+    let offsync = params.offsync ? params.offsync : false
     updateState({ guid, handle, node, name, location, description, imageUrl, cardId, status, offsync })
   }, [params])
 
-  const getStatusLabel = (card?: Card) => {
+  let getStatusLabel = (card?: Card) => {
     if (card) {
-      const { status, offsync } = card
+      let { status, offsync } = card
       if (status === 'confirmed') {
         return 'savedStatus'
       }
@@ -70,10 +70,10 @@ export function useProfile(params: ProfileParams) {
   }
 
   useEffect(() => {
-    const card = state.cards.find((card) => card.guid === state.guid)
-    const statusLabel = getStatusLabel(card)
+    let card = state.cards.find((card) => card.guid === state.guid)
+    let statusLabel = getStatusLabel(card)
     if (card) {
-      const { handle, node, name, location, description, imageUrl, cardId, status, offsync } = card
+      let { handle, node, name, location, description, imageUrl, cardId, status, offsync } = card
       updateState({ handle, node, name, location, description, imageUrl, cardId, status, offsync, statusLabel })
     } else {
       updateState({ cardId: null, status: '', offsync: false, statusLabel })
@@ -81,12 +81,12 @@ export function useProfile(params: ProfileParams) {
   }, [state.cards, state.guid])
 
   useEffect(() => {
-    const identity = app.state.session?.getIdentity()
-    const contact = app.state.session?.getContact()
-    const setCards = (cards: Card[]) => {
+    let identity = app.state.session?.getIdentity()
+    let contact = app.state.session?.getContact()
+    let setCards = (cards: Card[]) => {
       updateState({ cards })
     }
-    const setProfile = (profile: Profile) => {
+    let setProfile = (profile: Profile) => {
       updateState({ profile })
     }
     contact.addCardListener(setCards)
@@ -97,57 +97,57 @@ export function useProfile(params: ProfileParams) {
     }
   }, [])
 
-  const actions = {
+  let actions = {
     save: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.addCard(state.node, state.guid)
     },
     saveAndConnect: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.addAndConnectCard(state.node, state.guid)
     },
     remove: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.removeCard(state.cardId)
     },
     connect: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.connectCard(state.cardId)
     },
     disconnect: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.disconnectCard(state.cardId)
     },
     ignore: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.ignoreCard(state.cardId)
     },
     deny: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.denyCard(state.cardId)
     },
     confirm: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.confirmCard(state.cardId)
     },
     cancel: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.disconnectCard(state.cardId)
     },
     accept: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.connectCard(state.cardId)
     },
     resync: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.resyncCard(state.cardId)
     },
     block: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.setBlockedCard(state.cardId, true)
     },
     report: async () => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.flagCard(state.cardId)
     },
   }
