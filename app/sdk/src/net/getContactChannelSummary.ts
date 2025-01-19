8c20e0f34dc66d1a03d734177e95e8c3e9ec232d
vuong <vuongdo_777@yahoo.com>
2025-01-19 15:46:04 +0000

    Document dependencies - removing deprecated function

--- app/sdk/src/net/getContactChannelSummary.ts
+++ app/sdk/src/net/getContactChannelSummary.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { ChannelSummaryEntity } from '../entities';
 
 export async function getContactChannelSummary(server: string, secure: boolean, guid: string, token: string, channelId: string): Promise<ChannelSummaryEntity> {
-  let endpoint = `http${secure ? 's' : ''}://${server}/content/channels/${channelId}/summary?contact=${guid}.${token}`;
-  let summary = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const endpoint = `http${secure ? 's' : ''}://${server}/content/channels/${channelId}/summary?contact=${guid}.${token}`;
+  const summary = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(summary.status);
   return await summary.json();
 }
