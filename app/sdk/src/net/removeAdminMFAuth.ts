cfee228845feb5697b9784b4e82ba4d2e5eeea4a
vuong <vuongdo_777@yahoo.com>
2025-01-20 09:45:07 +0000

    Fix auth - refactoring code

--- app/sdk/src/net/removeAdminMFAuth.ts
+++ app/sdk/src/net/removeAdminMFAuth.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeAdminMFAuth(server: string, secure: boolean, token: string) {
-  let endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  const endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(status);
 }
 
