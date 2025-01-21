2369b2940bd14032694e4aa524cca5095866cc3b
vuong <vuongdo_777@yahoo.com>
2025-01-21 14:37:29 +0000

    Document styles - enhancing logs

--- app/sdk/src/net/removeChannel.ts
+++ app/sdk/src/net/removeChannel.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeChannel(node: string, secure: boolean, token: string, channelId: string): Promise<void> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}?agent=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}?agent=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(status);
 }
