9917faa1a0862c6c24ebc4e099652c1ed82dcbbe
vuong <vuongdo_777@yahoo.com>
2025-01-21 03:43:31 +0000

    Document auth - removing deprecated function

--- app/mobile/src/api/addChannel.js
+++ app/mobile/src/api/addChannel.js
@@ -3,8 +3,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 export async function addChannel(server, token, type, data, cards ) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
-  let params = { dataType: type, data: JSON.stringify(data), groups: [], cards };
-  let channel = await fetchWithTimeout(`${protocol}://${server}/content/channels?agent=${token}`, { method: 'POST', body: JSON.stringify(params)} );
+  const params = { dataType: type, data: JSON.stringify(data), groups: [], cards };
+  const channel = await fetchWithTimeout(`${protocol}://${server}/content/channels?agent=${token}`, { method: 'POST', body: JSON.stringify(params)} );
   checkResponse(channel);
   return await channel.json();
 }
