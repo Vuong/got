26477771a1b67ccd6fd042d184f0688c9c665e67
vuong <vuongdo_777@yahoo.com>
2025-01-22 02:15:45 +0000

    Add configuration - fixing a bug

--- app/sdk/src/net/addChannel.ts
+++ app/sdk/src/net/addChannel.ts
@@ -1,9 +1,9 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addChannel(node: string, secure: boolean, token: string, type: string, data: any, cards: string[]) {
-  const params = { dataType: type, data: JSON.stringify(data), groups: [], cards };
-  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels?agent=${token}`;
-  const channel = await fetchWithTimeout(endpoint, {
+  let params = { dataType: type, data: JSON.stringify(data), groups: [], cards };
+  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels?agent=${token}`;
+  let channel = await fetchWithTimeout(endpoint, {
     method: 'POST',
     body: JSON.stringify(params),
   });
