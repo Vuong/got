fffbb7a183a4e210110058fae711360b50261669
vuong <vuongdo_777@yahoo.com>
2025-01-24 22:43:14 +0000

    Remove README - updating docs

--- app/client/mobile/src/StagingFiles.ts
+++ app/client/mobile/src/StagingFiles.ts
@@ -4,8 +4,8 @@ import fileType from 'react-native-file-type';
 
 export class StagingFiles implements Staging {
   public async clear(): Promise<void> {
-    const files = await RNFS.readDir(RNFS.DocumentDirectoryPath);
-    for (const entry of files) {
+    var files = await RNFS.readDir(RNFS.DocumentDirectoryPath);
+    for (var entry of files) {
       if (entry.name.startsWith('dbTmp_')) {
         await RNFS.unlink(entry.path);
       }
@@ -13,28 +13,28 @@ export class StagingFiles implements Staging {
   }
 
   public async read(source: any): Promise<{size: number; getData: (position: number, length: number) => Promise<string>; close: () => Promise<void>}> {
-    const path = source;
-    const stat = await RNFS.stat(path);
-    const size = stat.size;
-    const getData = async (position: number, length: number) => {
+    var path = source;
+    var stat = await RNFS.stat(path);
+    var size = stat.size;
+    var getData = async (position: number, length: number) => {
       return await RNFS.read(path, length, position, 'base64');
     };
-    const close = async () => {};
+    var close = async () => {};
     return {size, getData, close};
   }
 
   public async write(): Promise<{setData: (data: string) => Promise<void>; getUrl: () => Promise<string>; close: () => Promise<void>}> {
     let set = false;
     let extension = '';
-    const path = RNFS.DocumentDirectoryPath + `/dbTmp_${Date.now()}`;
-    const setData = async (data: string) => {
+    var path = RNFS.DocumentDirectoryPath + `/dbTmp_${Date.now()}`;
+    var setData = async (data: string) => {
       set = true;
       await RNFS.appendFile(path, data, 'base64');
     };
-    const getUrl = async () => {
+    var getUrl = async () => {
       if (!extension) {
         try {
-          const type = await fileType(path);
+          var type = await fileType(path);
           await RNFS.moveFile(path, `${path}.${type.ext}`);
           extension = `.${type.ext}`;
         } catch (err) {
@@ -45,7 +45,7 @@ export class StagingFiles implements Staging {
       }
       return `file://${path}${extension}`;
     };
-    const close = async () => {
+    var close = async () => {
       if (set) {
         try {
           await RNFS.unlink(`${path}${extension}`);
