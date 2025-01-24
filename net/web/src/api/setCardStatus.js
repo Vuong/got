f18e124cdd31ca68f78e7acea23e84efd885cd40
vuong <vuongdo_777@yahoo.com>
2025-01-24 04:47:19 +0000

    Document build - removing deprecated function

--- net/web/src/api/setCardStatus.js
+++ net/web/src/api/setCardStatus.js
@@ -1,19 +1,19 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setCardConnecting(token, cardId) {
-  const card = await fetchWithTimeout(`/contact/cards/${cardId}/status?agent=${token}`, { method: 'PUT', body: JSON.stringify('connecting') } );
+  let card = await fetchWithTimeout(`/contact/cards/${cardId}/status?agent=${token}`, { method: 'PUT', body: JSON.stringify('connecting') } );
   checkResponse(card);
   return await card.json();
 }
 
 export async function setCardConnected(token, cardId, access, view, article, channel, profile) {
-  const card = await fetchWithTimeout(`/contact/cards/${cardId}/status?agent=${token}&token=${access}&viewRevision=${view}&articleRevision=${article}&channelRevision=${channel}&profileRevision=${profile}`, { method: 'PUT', body: JSON.stringify('connected') } );
+  let card = await fetchWithTimeout(`/contact/cards/${cardId}/status?agent=${token}&token=${access}&viewRevision=${view}&articleRevision=${article}&channelRevision=${channel}&profileRevision=${profile}`, { method: 'PUT', body: JSON.stringify('connected') } );
   checkResponse(card);
   return await card.json();
 }
 
 export async function setCardConfirmed(token, cardId) {
-  const card = await fetchWithTimeout(`/contact/cards/${cardId}/status?agent=${token}`, { method: 'PUT', body: JSON.stringify('confirmed') } );
+  let card = await fetchWithTimeout(`/contact/cards/${cardId}/status?agent=${token}`, { method: 'PUT', body: JSON.stringify('confirmed') } );
   checkResponse(card);
   return await card.json();
 }
