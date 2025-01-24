aa12c0c106eaf0b00bf8520e3befe74b7e5a3f0e
vuong <vuongdo_777@yahoo.com>
2025-01-24 08:26:36 +0000

    Update database - fixing a bug

--- net/web/src/api/setCardCloseMessage.js
+++ net/web/src/api/setCardCloseMessage.js
@@ -1,12 +1,12 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setCardCloseMessage(server, message) {
-  let host = "";
+  const host = "";
   if (server) {
     host = `https://${server}`;
   }
 
-  let status = await fetchWithTimeout(`${host}/contact/closeMessage`, { method: 'PUT', body: JSON.stringify(message) });
+  const status = await fetchWithTimeout(`${host}/contact/closeMessage`, { method: 'PUT', body: JSON.stringify(message) });
   checkResponse(status);
   return await status.json();
 }
