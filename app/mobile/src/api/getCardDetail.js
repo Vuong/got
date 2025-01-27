fe2aed8738e394cf18be31d4b0647eed56477b5c
vuong <vuongdo_777@yahoo.com>
2025-01-27 13:06:10 +0000

    Revert API - adding new feature

--- app/mobile/src/api/getCardDetail.js
+++ app/mobile/src/api/getCardDetail.js
@@ -3,8 +3,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 export async function getCardDetail(server, token, cardId) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
-  const param = "?agent=" + token
-  const detail = await fetchWithTimeout(`${protocol}://${server}/contact/cards/${cardId}/detail${param}`, { method: 'GET' });
+  let param = "?agent=" + token
+  let detail = await fetchWithTimeout(`${protocol}://${server}/contact/cards/${cardId}/detail${param}`, { method: 'GET' });
   checkResponse(detail);
   return await detail.json()
 }
