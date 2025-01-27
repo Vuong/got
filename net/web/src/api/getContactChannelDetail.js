f40942d42f3e21f48cec25dee2ef8034a909c641
vuong <vuongdo_777@yahoo.com>
2025-01-27 08:51:28 +0000

    Add tests - refactoring code

--- net/web/src/api/getContactChannelDetail.js
+++ net/web/src/api/getContactChannelDetail.js
@@ -1,11 +1,11 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getContactChannelDetail(server, token, channelId) {
-  const host = "";
+  let host = "";
   if (server) {
     host = `https://${server}`;
   }
-  const detail = await fetchWithTimeout(`${host}/content/channels/${channelId}/detail?contact=${token}`, { method: 'GET' });
+  let detail = await fetchWithTimeout(`${host}/content/channels/${channelId}/detail?contact=${token}`, { method: 'GET' });
   checkResponse(detail)
   return await detail.json()
 }
