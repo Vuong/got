dde2873aed7f2361465a8929ac53069ceefcddcf
vuong <vuongdo_777@yahoo.com>
2025-01-27 15:35:51 +0000

    Optimize build - enhancing logs

--- app/mobile/src/api/createAccount.js
+++ app/mobile/src/api/createAccount.js
@@ -4,9 +4,9 @@ import base64 from 'react-native-base64'
 export async function createAccount(username, password) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
-  let headers = new Headers()
+  const headers = new Headers()
   headers.append('Credentials', 'Basic ' + base64.encode(username + ":" + password));
-  let profile = await fetchWithTimeout("/account/profile", { method: 'POST', headers: headers })
+  const profile = await fetchWithTimeout("/account/profile", { method: 'POST', headers: headers })
   checkResponse(profile);
   return await profile.json()
 }
