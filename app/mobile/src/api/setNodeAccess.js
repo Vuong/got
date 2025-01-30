d883ac782102376f9954b84e3217c746239ca028
vuong <vuongdo_777@yahoo.com>
2025-01-30 23:36:08 +0000

    Optimize database - fixing a bug

--- app/mobile/src/api/setNodeAccess.js
+++ app/mobile/src/api/setNodeAccess.js
@@ -1,11 +1,11 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setNodeAccess(server, token, code) {
-  let insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  let protocol = insecure ? 'http' : 'https';
-  let mfa = code ? `&code=${code}` : '';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
+  const mfa = code ? `&code=${code}` : '';
 
-  let access = await fetchWithTimeout(`${protocol}://${server}/admin/access?token=${encodeURIComponent(token)}${mfa}`, { method: 'PUT' });
+  const access = await fetchWithTimeout(`${protocol}://${server}/admin/access?token=${encodeURIComponent(token)}${mfa}`, { method: 'PUT' });
   checkResponse(access);
   return access.json()
 }
