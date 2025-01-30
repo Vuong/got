632488307413ebc2e90d80f8f9d0a1a3c503bb44
vuong <vuongdo_777@yahoo.com>
2025-01-30 16:27:09 +0000

    Improve dependencies - improving security

--- app/sdk/src/net/getAdminMFAuth.ts
+++ app/sdk/src/net/getAdminMFAuth.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getAdminMFAuth(server: string, secure: boolean, token: string): Promise<boolean> {
-  let endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}`;
-  let mfa = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}`;
+  const mfa = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(mfa.status);
   return await mfa.json();
 }
