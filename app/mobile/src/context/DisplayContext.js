8c2d80cd9aa5329d034354d36605f3b25a48ed71
vuong <vuongdo_777@yahoo.com>
2025-01-31 10:06:36 +0000

    Add API - removing deprecated function

--- app/mobile/src/context/DisplayContext.js
+++ app/mobile/src/context/DisplayContext.js
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 import { useDisplayContext } from './useDisplayContext.hook';
 
-export const DisplayContext = createContext({});
+export let DisplayContext = createContext({});
 
 export function DisplayContextProvider({ children }) {
-  const { state, actions } = useDisplayContext();
+  let { state, actions } = useDisplayContext();
   return (
     <DisplayContext.Provider value={{ state, actions }}>
       {children}
