4c7a2833b079376f35bd96399c742b19d63fcd10
vuong <vuongdo_777@yahoo.com>
2025-01-31 09:14:06 +0000

    Refactor README - removing deprecated function

--- net/web/src/api/addAccountMFA.js
+++ net/web/src/api/addAccountMFA.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addAccountMFA(token) {
-  let mfa = await fetchWithTimeout(`/account/mfauth?agent=${token}`, { method: 'POST' })
+  const mfa = await fetchWithTimeout(`/account/mfauth?agent=${token}`, { method: 'POST' })
   checkResponse(mfa);
   return mfa.json();
 }
