121188782433c936ee3104ddf367c43d818327af
vuong <vuongdo_777@yahoo.com>
2025-01-31 22:50:09 +0000

    Improve styles - adding new feature

--- net/web/src/api/getContactChannelTopic.js
+++ net/web/src/api/getContactChannelTopic.js
@@ -1,12 +1,12 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getContactChannelTopic(server, token, channelId, topicId) {
-  let host = "";
+  const host = "";
   if (server) {
     host = `https://${server}`;
   }
 
-  let topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}/detail?contact=${token}`, 
+  const topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}/detail?contact=${token}`, 
     { method: 'GET' });
   checkResponse(topic)
   return await topic.json()
