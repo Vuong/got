91782eb0c8c0da0b25279a4c02c2da042948f2fc
vuong <vuongdo_777@yahoo.com>
2025-01-31 08:05:29 +0000

    Fix build - handling edge cases

--- net/web/src/api/addChannel.js
+++ net/web/src/api/addChannel.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addChannel(token, type, cards, data ) {
-  let params = { dataType: type, data: JSON.stringify(data), groups: [], cards };
-  let channel = await fetchWithTimeout(`/content/channels?agent=${token}`, { method: 'POST', body: JSON.stringify(params)} );
+  const params = { dataType: type, data: JSON.stringify(data), groups: [], cards };
+  const channel = await fetchWithTimeout(`/content/channels?agent=${token}`, { method: 'POST', body: JSON.stringify(params)} );
   checkResponse(channel);
   return await channel.json();
 }
