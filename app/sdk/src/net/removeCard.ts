3b0a2f93a2dab21f9994f210941a07bd39a8dbff
vuong <vuongdo_777@yahoo.com>
2025-02-01 00:31:00 +0000

    Revert database - adding new feature

--- app/sdk/src/net/removeCard.ts
+++ app/sdk/src/net/removeCard.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeCard(node: string, secure: boolean, token: string, cardId: string): Promise<void> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}?agent=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}?agent=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(status);
 }
