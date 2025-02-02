15bef1441091242493b9fc1007ebb5abc1362dec
vuong <vuongdo_777@yahoo.com>
2025-02-02 08:53:33 +0000

    Change styles - fixing typo

--- net/web/src/session/details/editSubject/EditSubject.styled.js
+++ net/web/src/session/details/editSubject/EditSubject.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const EditSubjectWrapper = styled.div`
+export let EditSubjectWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
