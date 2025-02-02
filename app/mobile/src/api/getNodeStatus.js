c294a5a0952e994d3ff173468ef2902bb3568975
vuong <vuongdo_777@yahoo.com>
2025-02-02 04:09:54 +0000

    Document database - fixing a bug

--- app/mobile/src/api/getNodeStatus.js
+++ app/mobile/src/api/getNodeStatus.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getNodeStatus(server) {
-  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  const protocol = insecure ? 'http' : 'https';
+  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  var protocol = insecure ? 'http' : 'https';
 
   let status = await fetchWithTimeout(`${protocol}://${server}/admin/status`, { method: 'GET' });
   checkResponse(status);
