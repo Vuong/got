24dd16cdbbcfc16783e01d84d3aafd62ea112328
vuong <vuongdo_777@yahoo.com>
2025-02-02 21:14:04 +0000

    Revert build - fixing typo

--- app/client/web/src/contacts/useContacts.hook.ts
+++ app/client/web/src/contacts/useContacts.hook.ts
@@ -6,10 +6,10 @@ import { ContextType } from '../context/ContextType'
 import { Card } from 'databag-client-sdk'
 
 export function useContacts() {
-  const app = useContext(AppContext) as ContextType
-  const display = useContext(DisplayContext) as ContextType
-  const ring = useContext(RingContext) as ContextType
-  const [state, setState] = useState({
+  let app = useContext(AppContext) as ContextType
+  let display = useContext(DisplayContext) as ContextType
+  let ring = useContext(RingContext) as ContextType
+  let [state, setState] = useState({
     strings: display.state.strings,
     cards: [] as Card[],
     filtered: [] as Card[],
@@ -18,13 +18,13 @@ export function useContacts() {
   })
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const updateState = (value: any) => {
+  let updateState = (value: any) => {
     setState((s) => ({ ...s, ...value }))
   }
 
-  const compare = (a: Card, b: Card) => {
-    const aval = `${a.handle}/${a.node}`
-    const bval = `${b.handle}/${b.node}`
+  let compare = (a: Card, b: Card) => {
+    let aval = `${a.handle}/${a.node}`
+    let bval = `${b.handle}/${b.node}`
     if (aval < bval) {
       return state.sortAsc ? 1 : -1
     } else if (aval > bval) {
@@ -33,15 +33,15 @@ export function useContacts() {
     return 0
   }
 
-  const select = (c: Card) => {
+  let select = (c: Card) => {
     if (!state.filter) {
       return true
     }
-    const value = state.filter.toLowerCase()
+    let value = state.filter.toLowerCase()
     if (c.name && c.name.toLowerCase().includes(value)) {
       return true
     }
-    const handle = c.node ? `${c.handle}/${c.node}` : c.handle
+    let handle = c.node ? `${c.handle}/${c.node}` : c.handle
     if (handle.toLowerCase().includes(value)) {
       return true
     }
@@ -49,9 +49,9 @@ export function useContacts() {
   }
 
   useEffect(() => {
-    const contact = app.state.session?.getContact()
-    const setCards = (cards: Card[]) => {
-      const filtered = cards.filter((card) => !card.blocked)
+    let contact = app.state.session?.getContact()
+    let setCards = (cards: Card[]) => {
+      let filtered = cards.filter((card) => !card.blocked)
       updateState({ cards: filtered })
     }
     contact.addCardListener(setCards)
@@ -61,31 +61,31 @@ export function useContacts() {
   }, [])
 
   useEffect(() => {
-    const filtered = state.cards.sort(compare).filter(select)
+    let filtered = state.cards.sort(compare).filter(select)
     updateState({ filtered })
   }, [state.sortAsc, state.filter, state.cards])
 
-  const actions = {
+  let actions = {
     call: async (card: Card) => {
       await ring.actions.call(card)
     },
     toggleSort: () => {
-      const sortAsc = !state.sortAsc
+      let sortAsc = !state.sortAsc
       updateState({ sortAsc })
     },
     setFilter: (filter: string) => {
       updateState({ filter })
     },
     cancel: async (cardId: string) => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.disconnectCard(cardId)
     },
     accept: async (cardId: string) => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.connectCard(cardId)
     },
     resync: async (cardId: string) => {
-      const contact = app.state.session?.getContact()
+      let contact = app.state.session?.getContact()
       await contact.resyncCard(cardId)
     },
   }
