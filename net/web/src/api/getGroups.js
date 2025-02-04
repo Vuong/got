22ff8ccabe54800aa037e13f399ae69a7241bfb3
vuong <vuongdo_777@yahoo.com>
2025-02-04 18:09:42 +0000

    Optimize auth - removing deprecated function

--- net/web/src/api/getGroups.js
+++ net/web/src/api/getGroups.js
@@ -1,11 +1,11 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getGroups(token, revision) {
-  let param = "agent=" + token
+  const param = "agent=" + token
   if (revision != null) {
     param += '&revision=' + revision
   }
-  let groups = await fetchWithTimeout(`/alias/groups?${param}`, { method: 'GET' });
+  const groups = await fetchWithTimeout(`/alias/groups?${param}`, { method: 'GET' });
   checkResponse(groups)
   return await groups.json()
 }
