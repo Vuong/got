34f90c0f126944b2cfecba52ef1caef8bf2c8b9a
vuong <vuongdo_777@yahoo.com>
2025-02-04 16:52:29 +0000

    Add auth - fixing a bug

--- net/web/test/Person.test.js
+++ net/web/test/Person.test.js
@@ -8,16 +8,16 @@ import * as fetchUtil from 'api/fetchUtil';
 let contactHook;
 let cardContext;
 function ContactView() {
-  const [name, setName] = useState();
-  const [status, setStatus] = useState();
-  const { state, actions } = useContact('01ab23');
-  const [renderCount, setRenderCount] = useState(0);
-  const card = useContext(CardContext);
+  var [name, setName] = useState();
+  var [status, setStatus] = useState();
+  var { state, actions } = useContact('01ab23');
+  var [renderCount, setRenderCount] = useState(0);
+  var card = useContext(CardContext);
   cardContext = card;
   contactHook = actions;
 
   useEffect(() => {
-    const rendered = [];
+    var rendered = [];
     setName(state.name);
     setStatus(state.status);
     setRenderCount(renderCount + 1);
@@ -44,14 +44,14 @@ function ContactTestApp() {
 let fetchCards;
 let fetchProfile;
 let fetchDetail;
-const realFetchWithTimeout = fetchUtil.fetchWithTimeout;
-const realFetchWithCustomTimeout = fetchUtil.fetchWithCustomTimeout;
+var realFetchWithTimeout = fetchUtil.fetchWithTimeout;
+var realFetchWithCustomTimeout = fetchUtil.fetchWithCustomTimeout;
 beforeEach(() => {
   fetchCards = [];
   fetchDetail = {};
   fetchProfile = {};
 
-  const mockFetch = jest.fn().mockImplementation((url, options) => {
+  var mockFetch = jest.fn().mockImplementation((url, options) => {
 
     if (url.startsWith('/contact/cards?agent')) {
       return Promise.resolve({
