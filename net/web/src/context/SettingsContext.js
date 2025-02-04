864ed461ae80a8c5dab5e2edabe022b8c4b23191
vuong <vuongdo_777@yahoo.com>
2025-02-04 06:15:32 +0000

    Change styles - refactoring code

--- net/web/src/context/SettingsContext.js
+++ net/web/src/context/SettingsContext.js
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 import { useSettingsContext } from './useSettingsContext.hook';
 
-export const SettingsContext = createContext({});
+export let SettingsContext = createContext({});
 
 export function SettingsContextProvider({ children }) {
-  const { state, actions } = useSettingsContext();
+  let { state, actions } = useSettingsContext();
   return (
     <SettingsContext.Provider value={{ state, actions }}>
       {children}
