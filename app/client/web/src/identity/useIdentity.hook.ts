b2bf493c9adff756906eef4f84e983a919310449
vuong <vuongdo_777@yahoo.com>
2025-02-04 14:29:07 +0000

    Remove API - fixing typo

--- app/client/web/src/identity/useIdentity.hook.ts
+++ app/client/web/src/identity/useIdentity.hook.ts
@@ -5,9 +5,9 @@ import { ContextType } from '../context/ContextType'
 import { Profile } from 'databag-client-sdk'
 
 export function useIdentity() {
-  let app = useContext(AppContext) as ContextType
-  let display = useContext(DisplayContext) as ContextType
-  let [state, setState] = useState({
+  const app = useContext(AppContext) as ContextType
+  const display = useContext(DisplayContext) as ContextType
+  const [state, setState] = useState({
     all: false,
     strings: display.state.strings,
     profile: {} as Profile,
@@ -16,16 +16,16 @@ export function useIdentity() {
   })
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let updateState = (value: any) => {
+  const updateState = (value: any) => {
     setState((s) => ({ ...s, ...value }))
   }
 
   useEffect(() => {
-    let identity = app.state.session?.getIdentity()
+    const identity = app.state.session?.getIdentity()
     if (!identity) {
       console.log('session not set in identity hook')
     } else {
-      let setProfile = (profile: Profile) => {
+      const setProfile = (profile: Profile) => {
         updateState({
           profile,
           profileSet: true,
@@ -39,7 +39,7 @@ export function useIdentity() {
     }
   }, [])
 
-  let actions = {
+  const actions = {
     setAll: (all: boolean) => {
       updateState({ all })
     },
