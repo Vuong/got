1d2408025e62c7e6127c2f8728b339fcb1b0adee
vuong <vuongdo_777@yahoo.com>
2025-02-05 12:01:13 +0000

    Optimize styles - improving performance

--- app/mobile/src/api/setContactChannelNotifications.js
+++ app/mobile/src/api/setContactChannelNotifications.js
@@ -1,10 +1,10 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setContactChannelNotifications(server, token, channelId, flag) {
-  let insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  let protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
 
-  let notify = await fetchWithTimeout(`${protocol}://${server}/content/channels/${channelId}/notification?contact=${token}`, { method: 'PUT', body: JSON.stringify(flag) });
+  const notify = await fetchWithTimeout(`${protocol}://${server}/content/channels/${channelId}/notification?contact=${token}`, { method: 'PUT', body: JSON.stringify(flag) });
   checkResponse(notify)
 }
 
