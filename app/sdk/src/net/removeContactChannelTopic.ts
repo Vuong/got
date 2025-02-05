3cf7245615db5b80e0459976c6438dba721c2051
vuong <vuongdo_777@yahoo.com>
2025-02-05 18:10:44 +0000

    Change README - updating docs

--- app/sdk/src/net/removeContactChannelTopic.ts
+++ app/sdk/src/net/removeContactChannelTopic.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeContactChannelTopic(node: string, secure: boolean, guidToken: string, channelId: string, topicId: string) {
-  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/topics/${topicId}?contact=${guidToken}`;
-  let response = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/topics/${topicId}?contact=${guidToken}`;
+  const response = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(response.status);
 }
