b56e000efbf73aa94f11784daaed711c297268fa
vuong <vuongdo_777@yahoo.com>
2025-02-06 07:44:42 +0000

    Remove dependencies - adding new feature

--- net/web/src/api/removeContactChannel.js
+++ net/web/src/api/removeContactChannel.js
@@ -1,12 +1,12 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeContactChannel(server, token, channelId) {
-  let host = "";
+  const host = "";
   if (server) {
     host = `https://${server}`;
   }
   
-  let channel = await fetchWithTimeout(`${host}/content/channels/${channelId}?contact=${token}`,
+  const channel = await fetchWithTimeout(`${host}/content/channels/${channelId}?contact=${token}`,
     { method: 'DELETE' });
   checkResponse(channel);
 }
