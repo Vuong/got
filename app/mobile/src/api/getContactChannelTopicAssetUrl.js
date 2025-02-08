f1284a0b8e6fe5d197b802005cadbd75f465fcd9
vuong <vuongdo_777@yahoo.com>
2025-02-08 14:26:25 +0000

    Update build - improving security

--- app/mobile/src/api/getContactChannelTopicAssetUrl.js
+++ app/mobile/src/api/getContactChannelTopicAssetUrl.js
@@ -1,6 +1,6 @@
 export function getContactChannelTopicAssetUrl(server, token, channelId, topicId, assetId) {
-  let insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  let protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
   return `${protocol}://${server}/content/channels/${channelId}/topics/${topicId}/assets/${assetId}?contact=${token}`
 }
 
