1db880ba17e8377fdc2d3b57d48619b9a34b1af8
vuong <vuongdo_777@yahoo.com>
2025-02-08 01:25:33 +0000

    Optimize README - handling edge cases

--- app/sdk/src/net/addNodeAccount.ts
+++ app/sdk/src/net/addNodeAccount.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addNodeAccount(server: string, secure: boolean, token: string): Promise<string> {
-  const endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts?token=${token}`;
-  const create = await fetchWithTimeout(endpoint, { method: 'POST' });
+  let endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts?token=${token}`;
+  let create = await fetchWithTimeout(endpoint, { method: 'POST' });
   checkResponse(create.status);
   return await create.json();
 }
