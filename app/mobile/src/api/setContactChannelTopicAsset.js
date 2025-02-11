1d1d085ba72c626fd08dec7a4f97327b99dc5181
vuong <vuongdo_777@yahoo.com>
2025-02-11 23:28:33 +0000

    Update auth - adding new feature

--- app/mobile/src/api/setContactChannelTopicAsset.js
+++ app/mobile/src/api/setContactChannelTopicAsset.js
@@ -4,7 +4,7 @@ export async function setContactChannelTopicSubject(server, token, channelId, to
   const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   const protocol = insecure ? 'http' : 'https';
 
-  var host = "";
+  let host = "";
   if (server) {
     host = `${protocol}://${server}`;
   }
@@ -12,10 +12,10 @@ export async function setContactChannelTopicSubject(server, token, channelId, to
   if (asset.image) {
     const formData = new FormData();
     formData.append('asset', asset.image);
-    var transform = encodeURIComponent(JSON.stringify(["ithumb;photo", "icopy;photo"]));
-    var topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
+    let transform = encodeURIComponent(JSON.stringify(["ithumb;photo", "icopy;photo"]));
+    let topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
     checkResponse(topicAsset);
-    var assetEntry = await topicAsset.json();
+    let assetEntry = await topicAsset.json();
     return {
       image: {
         thumb: assetEntry.find(item => item.transform === 'ithumb;photo').assetId,
@@ -26,11 +26,11 @@ export async function setContactChannelTopicSubject(server, token, channelId, to
   else if (asset.video) {
     const formData = new FormData();
     formData.append('asset', asset.video);
-    var thumb = "vthumb;video;" + asset.position
-    var transform = encodeURIComponent(JSON.stringify(["vhd;video", "vlq;video", thumb]));
-    var topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
+    let thumb = "vthumb;video;" + asset.position
+    let transform = encodeURIComponent(JSON.stringify(["vhd;video", "vlq;video", thumb]));
+    let topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
     checkResponse(topicAsset);
-    var assetEntry = await topicAsset.json();
+    let assetEntry = await topicAsset.json();
     return {
       video: {
         thumb: assetEntry.find(item => item.transform === thumb).assetId,
@@ -42,10 +42,10 @@ export async function setContactChannelTopicSubject(server, token, channelId, to
   else if (asset.audio) {
     const formData = new FormData();
     formData.append('asset', asset.audio);
-    var transform = encodeURIComponent(JSON.stringify(["acopy;audio"]));
-    var topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
+    let transform = encodeURIComponent(JSON.stringify(["acopy;audio"]));
+    let topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
     checkResponse(topicAsset);
-    var assetEntry = await topicAsset.json();
+    let assetEntry = await topicAsset.json();
     return {
       audio: {
         label: asset.label,
