aab1599cfb386d33857c59de3d401a02b446792a
vuong <vuongdo_777@yahoo.com>
2025-02-12 19:25:01 +0000

    Document dependencies - updating docs

--- app/sdk/src/net/setProfileImage.ts
+++ app/sdk/src/net/setProfileImage.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setProfileImage(node: string, secure: boolean, token: string, image: string) {
-  let endpoint = `http${secure ? 's' : ''}://${node}/profile/image?agent=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, {
+  const endpoint = `http${secure ? 's' : ''}://${node}/profile/image?agent=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, {
     method: 'PUT',
     body: JSON.stringify(image),
   });
