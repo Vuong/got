4245adf86287b722d0ee3ffc8c0c20abbc8af198
vuong <vuongdo_777@yahoo.com>
2025-02-12 20:56:47 +0000

    Fix auth - adding new feature

--- net/web/src/api/getGroups.js
+++ net/web/src/api/getGroups.js
@@ -1,11 +1,11 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getGroups(token, revision) {
-  const param = "agent=" + token
+  let param = "agent=" + token
   if (revision != null) {
     param += '&revision=' + revision
   }
-  const groups = await fetchWithTimeout(`/alias/groups?${param}`, { method: 'GET' });
+  let groups = await fetchWithTimeout(`/alias/groups?${param}`, { method: 'GET' });
   checkResponse(groups)
   return await groups.json()
 }
