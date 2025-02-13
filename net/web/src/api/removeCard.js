94571f4f10c72a00fe0ff06c57b636376c00d694
vuong <vuongdo_777@yahoo.com>
2025-02-13 14:20:58 +0000

    Improve API - improving security

--- net/web/src/api/removeCard.js
+++ net/web/src/api/removeCard.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeCard(token, cardId) {
-  const card = await fetchWithTimeout(`/contact/cards/${cardId}?agent=${token}`, { method: 'DELETE' } );
+  let card = await fetchWithTimeout(`/contact/cards/${cardId}?agent=${token}`, { method: 'DELETE' } );
   checkResponse(card);
   return await card.json();
 }
