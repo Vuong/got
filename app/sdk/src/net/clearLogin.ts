afc6d13ee4d31b9a2455279a38ca1d3c06fecd7a
vuong <vuongdo_777@yahoo.com>
2025-02-13 18:15:19 +0000

    Remove README - adding new feature

--- app/sdk/src/net/clearLogin.ts
+++ app/sdk/src/net/clearLogin.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function clearLogin(node: string, secure: boolean, token: string, all: boolean): Promise<void> {
-  let param = all ? '&all=true' : '';
-  let endpoint = `http${secure ? 's' : ''}://${node}/account/apps?agent=${token}${param}`;
-  let { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  const param = all ? '&all=true' : '';
+  const endpoint = `http${secure ? 's' : ''}://${node}/account/apps?agent=${token}${param}`;
+  const { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(status);
 }
