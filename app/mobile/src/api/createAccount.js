e9ca4d8b2850d3f26f2b2e1bb59304662c82d2ae
vuong <vuongdo_777@yahoo.com>
2025-02-14 13:18:35 +0000

    Update tests - fixing typo

--- app/mobile/src/api/createAccount.js
+++ app/mobile/src/api/createAccount.js
@@ -4,9 +4,9 @@ import base64 from 'react-native-base64'
 export async function createAccount(username, password) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
-  const headers = new Headers()
+  let headers = new Headers()
   headers.append('Credentials', 'Basic ' + base64.encode(username + ":" + password));
-  const profile = await fetchWithTimeout("/account/profile", { method: 'POST', headers: headers })
+  let profile = await fetchWithTimeout("/account/profile", { method: 'POST', headers: headers })
   checkResponse(profile);
   return await profile.json()
 }
