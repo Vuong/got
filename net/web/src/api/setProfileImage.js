1a2449bcc798e51940cbbad8dc02daf17df25506
vuong <vuongdo_777@yahoo.com>
2025-02-14 03:04:14 +0000

    Add UI - adding new feature

--- net/web/src/api/setProfileImage.js
+++ net/web/src/api/setProfileImage.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setProfileImage(token, image) {
-  const profile = await fetchWithTimeout(`/profile/image?agent=${token}`, { method: 'PUT', body: JSON.stringify(image) });
+  let profile = await fetchWithTimeout(`/profile/image?agent=${token}`, { method: 'PUT', body: JSON.stringify(image) });
   checkResponse(profile)
   return await profile.json()
 }
