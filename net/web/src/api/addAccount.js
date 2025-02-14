c03c1b616e9ae39ac682c70f8974e23675a17cc3
vuong <vuongdo_777@yahoo.com>
2025-02-14 03:06:35 +0000

    Document configuration - enhancing logs

--- net/web/src/api/addAccount.js
+++ net/web/src/api/addAccount.js
@@ -2,13 +2,13 @@ import { checkResponse, fetchWithCustomTimeout } from './fetchUtil';
 var base64 = require('base-64');
 
 export async function addAccount(username, password, token) {
-  const access = "";
+  let access = "";
   if (token) {
     access = `?token=${token}`
   }
-  const headers = new Headers()
+  let headers = new Headers()
   headers.append('Credentials', 'Basic ' + base64.encode(username + ":" + password));
-  const profile = await fetchWithCustomTimeout(`/account/profile${access}`, { method: 'POST', headers: headers }, 60000)
+  let profile = await fetchWithCustomTimeout(`/account/profile${access}`, { method: 'POST', headers: headers }, 60000)
   checkResponse(profile);
   return await profile.json()
 }
