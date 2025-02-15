cf6c3849d755171d2398d30c7876c5f380caa553
vuong <vuongdo_777@yahoo.com>
2025-02-15 22:13:30 +0000

    Optimize database - removing deprecated function

--- app/client/mobile/src/profile/Profile.styled.ts
+++ app/client/mobile/src/profile/Profile.styled.ts
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native';
 import {Colors} from '../constants/Colors';
 
-export const styles = StyleSheet.create({
+export let styles = StyleSheet.create({
   profile: {
     width: '100%',
     height: '100%',
