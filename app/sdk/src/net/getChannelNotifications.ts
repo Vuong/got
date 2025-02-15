d28710b179b4fe14eccf88077da90ba3e4a418a7
vuong <vuongdo_777@yahoo.com>
2025-02-15 10:13:49 +0000

    Document database - improving security

--- app/sdk/src/net/getChannelNotifications.ts
+++ app/sdk/src/net/getChannelNotifications.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getChannelNotifications(node: string, secure: boolean, token: string, channelId: string) {
-  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/notification?agent=${token}`;
-  let notify = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/notification?agent=${token}`;
+  const notify = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(notify.status);
   return await notify.json();
 }
