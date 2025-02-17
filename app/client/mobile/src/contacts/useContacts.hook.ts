57cbedc478a4818419694ecb4092b64c7bfbc231
vuong <vuongdo_777@yahoo.com>
2025-02-17 01:22:52 +0000

    Revert dependencies - improving performance

--- app/client/mobile/src/contacts/useContacts.hook.ts
+++ app/client/mobile/src/contacts/useContacts.hook.ts
@@ -6,10 +6,10 @@ import {ContextType} from '../context/ContextType';
 import {Card} from 'databag-client-sdk';
 
 export function useContacts() {
-  let app = useContext(AppContext) as ContextType;
-  let ring = useContext(RingContext) as ContextType;
-  let display = useContext(DisplayContext) as ContextType;
-  let [state, setState] = useState({
+  const app = useContext(AppContext) as ContextType;
+  const ring = useContext(RingContext) as ContextType;
+  const display = useContext(DisplayContext) as ContextType;
+  const [state, setState] = useState({
     layout: '',
     strings: display.state.strings,
     cards: [] as Card[],
@@ -18,19 +18,19 @@ export function useContacts() {
     filter: '',
   });
 
-  let updateState = (value: any) => {
+  const updateState = (value: any) => {
     setState(s => ({...s, ...value}));
   };
 
   useEffect(() => {
-    let {layout} = display.state;
+    const {layout} = display.state;
     updateState({layout});
   }, [display.state]);
 
   useEffect(() => {
-    let contact = app.state.session?.getContact();
-    let setCards = (cards: Card[]) => {
-      let filtered = cards.filter(card => !card.blocked);
+    const contact = app.state.session?.getContact();
+    const setCards = (cards: Card[]) => {
+      const filtered = cards.filter(card => !card.blocked);
       updateState({cards: filtered});
     };
     contact.addCardListener(setCards);
@@ -41,9 +41,9 @@ export function useContacts() {
   }, []);
 
   useEffect(() => {
-    let compare = (a: Card, b: Card) => {
-      let aval = `${a.handle}/${a.node}`;
-      let bval = `${b.handle}/${b.node}`;
+    const compare = (a: Card, b: Card) => {
+      const aval = `${a.handle}/${a.node}`;
+      const bval = `${b.handle}/${b.node}`;
       if (aval < bval) {
         return state.sortAsc ? 1 : -1;
       } else if (aval > bval) {
@@ -51,27 +51,27 @@ export function useContacts() {
       }
       return 0;
     };
-    let select = (c: Card) => {
+    const select = (c: Card) => {
       if (!state.filter) {
         return true;
       }
-      let value = state.filter.toLowerCase();
+      const value = state.filter.toLowerCase();
       if (c.name && c.name.toLowerCase().includes(value)) {
         return true;
       }
-      let handle = c.node ? `${c.handle}/${c.node}` : c.handle;
+      const handle = c.node ? `${c.handle}/${c.node}` : c.handle;
       if (handle.toLowerCase().includes(value)) {
         return true;
       }
       return false;
     };
-    let filtered = state.cards.sort(compare).filter(select);
+    const filtered = state.cards.sort(compare).filter(select);
     updateState({filtered});
   }, [state.sortAsc, state.filter, state.cards]);
 
-  let actions = {
+  const actions = {
     toggleSort: () => {
-      let sortAsc = !state.sortAsc;
+      const sortAsc = !state.sortAsc;
       updateState({sortAsc});
     },
     setFilter: (filter: string) => {
@@ -84,15 +84,15 @@ export function useContacts() {
       console.log('text', cardId);
     },
     cancel: async (cardId: string) => {
-      let contact = app.state.session?.getContact();
+      const contact = app.state.session?.getContact();
       await contact.disconnectCard(cardId);
     },
     accept: async (cardId: string) => {
-      let contact = app.state.session?.getContact();
+      const contact = app.state.session?.getContact();
       await contact.connectCard(cardId);
     },
     resync: async (cardId: string) => {
-      let contact = app.state.session?.getContact();
+      const contact = app.state.session?.getContact();
       await contact.resyncCard(cardId);
     },
   };
