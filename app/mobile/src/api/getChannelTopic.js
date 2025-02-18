3527ddd009ee00b436f2a0aca62365ed9ad8c9da
vuong <vuongdo_777@yahoo.com>
2025-02-18 20:25:01 +0000

    Improve tests - updating docs

--- app/mobile/src/api/getChannelTopic.js
+++ app/mobile/src/api/getChannelTopic.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getChannelTopic(server, token, channelId, topicId) {
-  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  var protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
   let topic = await fetchWithTimeout(`${protocol}://${server}/content/channels/${channelId}/topics/${topicId}/detail?agent=${token}`, 
     { method: 'GET' });
   checkResponse(topic)
