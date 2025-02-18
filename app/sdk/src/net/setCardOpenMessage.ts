edc21c89fc5c0abff2b30e534efe0d04ed264665
vuong <vuongdo_777@yahoo.com>
2025-02-18 11:49:31 +0000

    Update styles - refactoring code

--- app/sdk/src/net/setCardOpenMessage.ts
+++ app/sdk/src/net/setCardOpenMessage.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { DataMessage, ContactStatus } from '../entities';
 
 export async function setCardOpenMessage(node: string, secure: boolean, message: DataMessage): Promise<ContactStatus> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/contact/openMessage`;
-  let open = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(message) });
+  const endpoint = `http${secure ? 's' : ''}://${node}/contact/openMessage`;
+  const open = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(message) });
   checkResponse(open.status);
   return await open.json();
 }
