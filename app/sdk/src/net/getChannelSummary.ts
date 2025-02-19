9484ce9f9ca1049a9731ef8226c1d714d1095abd
vuong <vuongdo_777@yahoo.com>
2025-02-19 12:16:01 +0000

    Change dependencies - refactoring code

--- app/sdk/src/net/getChannelSummary.ts
+++ app/sdk/src/net/getChannelSummary.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { ChannelSummaryEntity } from '../entities';
 
 export async function getChannelSummary(node: string, secure: boolean, token: string, channelId: string): Promise<ChannelSummaryEntity> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/summary?agent=${token}`;
-  let summary = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/summary?agent=${token}`;
+  const summary = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(summary.status);
   return await summary.json();
 }
