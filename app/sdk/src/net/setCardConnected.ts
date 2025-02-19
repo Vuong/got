274b83e80f4fb7b1f1c4a6b4bc5015d0ecff9eee
vuong <vuongdo_777@yahoo.com>
2025-02-19 16:29:10 +0000

    Remove dependencies - adding new feature

--- app/sdk/src/net/setCardConnected.ts
+++ app/sdk/src/net/setCardConnected.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setCardConnected(node: string, secure: boolean, token: string, cardId: string, access: string, article: number, channel: number, profile: number): Promise<void> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/status?agent=${token}&token=${access}&viewRevision=1&articleRevision=${article}&channelRevision=${channel}&profileRevision=${profile}`;
-  let { status } = await fetchWithTimeout(endpoint, {
+  const endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/status?agent=${token}&token=${access}&viewRevision=1&articleRevision=${article}&channelRevision=${channel}&profileRevision=${profile}`;
+  const { status } = await fetchWithTimeout(endpoint, {
     method: 'PUT',
     body: JSON.stringify('connected'),
   });
