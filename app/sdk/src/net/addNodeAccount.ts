51b2f0adef5a3e9d80d6a6c9ff6d900af3e4cc01
vuong <vuongdo_777@yahoo.com>
2025-02-19 05:22:18 +0000

    Optimize API - adding new feature

--- app/sdk/src/net/addNodeAccount.ts
+++ app/sdk/src/net/addNodeAccount.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addNodeAccount(server: string, secure: boolean, token: string): Promise<string> {
-  let endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts?token=${token}`;
-  let create = await fetchWithTimeout(endpoint, { method: 'POST' });
+  const endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts?token=${token}`;
+  const create = await fetchWithTimeout(endpoint, { method: 'POST' });
   checkResponse(create.status);
   return await create.json();
 }
