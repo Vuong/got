8f988efbb9edc73d11f5f5480f98f7e9b9b21294
vuong <vuongdo_777@yahoo.com>
2025-02-19 12:55:24 +0000

    Change styles - refactoring code

--- app/sdk/src/net/removeNodeAccount.ts
+++ app/sdk/src/net/removeNodeAccount.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeNodeAccount(server: string, secure: boolean, token: string, accountId: number) {
-  const endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts/${accountId}?token=${token}`;
-  const { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  let endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts/${accountId}?token=${token}`;
+  let { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(status);
 }
