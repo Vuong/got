2679788ead251ab61746cc4239cc8d7e13858b97
vuong <vuongdo_777@yahoo.com>
2025-02-20 05:36:18 +0000

    Document auth - refactoring code

--- app/sdk/src/net/getContactChannelTopicDetail.ts
+++ app/sdk/src/net/getContactChannelTopicDetail.ts
@@ -2,10 +2,10 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { TopicDetailEntity } from '../entities';
 
 export async function getContactChannelTopicDetail(node: string, secure: boolean, guidToken: string, channelId: string, topicId: string): Promise<TopicDetailEntity> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/topics/${topicId}/detail?contact=${guidToken}`;
-  let detail = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/topics/${topicId}/detail?contact=${guidToken}`;
+  const detail = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(detail.status);
-  let topic = await detail.json();
+  const topic = await detail.json();
   if (!topic?.data?.topicDetail) {
     throw new Error('missing topic detail');
   } else {
