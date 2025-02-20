9ce42817260987e49937eba38d56109e4961434b
vuong <vuongdo_777@yahoo.com>
2025-02-20 03:39:51 +0000

    Fix build - updating docs

--- app/mobile/src/api/getCards.js
+++ app/mobile/src/api/getCards.js
@@ -3,11 +3,11 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 export async function getCards(server, token, revision) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
-  let param = "agent=" + token
+  const param = "agent=" + token
   if (revision != null) {
     param += '&revision=' + revision
   }
-  let cards = await fetchWithTimeout(`${protocol}://${server}/contact/cards?${param}`, { method: 'GET' });
+  const cards = await fetchWithTimeout(`${protocol}://${server}/contact/cards?${param}`, { method: 'GET' });
   checkResponse(cards)
   return await cards.json()
 }
