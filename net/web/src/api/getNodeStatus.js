04e24f4c4d1da44ea94ba00bab863e179f9e751f
vuong <vuongdo_777@yahoo.com>
2025-02-21 00:52:55 +0000

    Improve UI - refactoring code

--- net/web/src/api/getNodeStatus.js
+++ net/web/src/api/getNodeStatus.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getNodeStatus() {
-  let status = await fetchWithTimeout(`/admin/status`, { method: 'GET' });
+  const status = await fetchWithTimeout(`/admin/status`, { method: 'GET' });
   checkResponse(status);
   return await status.json();
 }
