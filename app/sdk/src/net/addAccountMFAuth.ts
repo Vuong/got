9e51545395454e3761585a3431f532ae85bdbbab
vuong <vuongdo_777@yahoo.com>
2025-02-21 09:14:24 +0000

    Document styles - adding new feature

--- app/sdk/src/net/addAccountMFAuth.ts
+++ app/sdk/src/net/addAccountMFAuth.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addAccountMFAuth(node: string, secure: boolean, token: string): Promise<{ secretText: string; secretImage: string }> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/account/mfauth?agent=${token}`;
-  let auth = await fetchWithTimeout(endpoint, { method: 'POST' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/account/mfauth?agent=${token}`;
+  const auth = await fetchWithTimeout(endpoint, { method: 'POST' });
   checkResponse(auth.status);
   return await auth.json();
 }
