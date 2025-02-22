ab495a11580b9e89bf733633ac043749000e9096
vuong <vuongdo_777@yahoo.com>
2025-02-22 12:07:50 +0000

    Update API - fixing a bug

--- app/sdk/src/net/setContactChannelTopicSubject.ts
+++ app/sdk/src/net/setContactChannelTopicSubject.ts
@@ -1,9 +1,9 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setContactChannelTopicSubject(node: string, secure: boolean, guidToken: string, channelId: string, topicId: string, dataType: string, data: any) {
-  let subject = { data: JSON.stringify(data), dataType };
-  let endpoint = `http${secure ? 's' : '' }://${node}/content/channels/${channelId}/topics/${topicId}/subject?contact=${guidToken}&confirm=true`;
-  let { status } = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(subject) });
+  const subject = { data: JSON.stringify(data), dataType };
+  const endpoint = `http${secure ? 's' : '' }://${node}/content/channels/${channelId}/topics/${topicId}/subject?contact=${guidToken}&confirm=true`;
+  const { status } = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(subject) });
   checkResponse(status);
 }   
 
