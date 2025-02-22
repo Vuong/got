58c1c375fc8d9ac31e29cbb95405ae916b42dd6d
vuong <vuongdo_777@yahoo.com>
2025-02-22 21:59:23 +0000

    Document dependencies - refactoring code

--- app/mobile/src/context/useProfileContext.hook.js
+++ app/mobile/src/context/useProfileContext.hook.js
@@ -7,7 +7,7 @@ import { getHandle } from 'api/getHandle';
 import { StoreContext } from 'context/StoreContext';
 
 export function useProfileContext() {
-  let [state, setState] = useState({
+  const [state, setState] = useState({
     offsync: false,
     identity: {},
     server: null,
@@ -15,29 +15,29 @@ export function useProfileContext() {
     monthLast: false,
     timeFull: false,
   });
-  let store = useContext(StoreContext);
+  const store = useContext(StoreContext);
 
-  let HOUR_KEY = 'hour';
-  let DATE_KEY = 'date';
+  const HOUR_KEY = 'hour';
+  const DATE_KEY = 'date';
 
-  let access = useRef(null);
-  let curRevision = useRef(null);
-  let setRevision = useRef(null);
-  let syncing = useRef(false);
+  const access = useRef(null);
+  const curRevision = useRef(null);
+  const setRevision = useRef(null);
+  const syncing = useRef(false);
 
-  let updateState = (value) => {
+  const updateState = (value) => {
     setState((s) => ({ ...s, ...value }))
   }
 
-  let sync = async () => {
+  const sync = async () => {
     if (access.current && !syncing.current && setRevision.current !== curRevision.current) {
       syncing.current = true;
 
       try {
-        let revision = curRevision.current;
-        let { server, token, guid } = access.current || {};
-        let identity = await getProfile(server, token);
-        let imageUrl = identity?.image ? getProfileImageUrl(server, token, revision) : null;
+        const revision = curRevision.current;
+        const { server, token, guid } = access.current || {};
+        const identity = await getProfile(server, token);
+        const imageUrl = identity?.image ? getProfileImageUrl(server, token, revision) : null;
         await store.actions.setProfile(guid, identity);
         await store.actions.setProfileRevision(guid, revision);
         updateState({ offsync: false, identity, imageUrl });
@@ -55,14 +55,14 @@ export function useProfileContext() {
     }
   };
 
-  let actions = {
+  const actions = {
     setSession: async (session) => {
-      let { guid, server, token } = session || {};
-      let identity = await store.actions.getProfile(guid);
-      let revision = await store.actions.getProfileRevision(guid);
-      let timeFull = (await store.actions.getAppValue(guid, HOUR_KEY)).set == true;
-      let monthLast = (await store.actions.getAppValue(guid, DATE_KEY)).set == true;
-      let imageUrl = identity?.image ? getProfileImageUrl(server, token, revision) : null;
+      const { guid, server, token } = session || {};
+      const identity = await store.actions.getProfile(guid);
+      const revision = await store.actions.getProfileRevision(guid);
+      const timeFull = (await store.actions.getAppValue(guid, HOUR_KEY)).set == true;
+      const monthLast = (await store.actions.getAppValue(guid, DATE_KEY)).set == true;
+      const imageUrl = identity?.image ? getProfileImageUrl(server, token, revision) : null;
       updateState({ offsync: false, identity, imageUrl, server, timeFull, monthLast });
       setRevision.current = revision;
       curRevision.current = revision;
@@ -76,24 +76,24 @@ export function useProfileContext() {
       sync();
     },
     setProfileData: async (name, location, description) => {
-      let { server, token } = access.current || {};
+      const { server, token } = access.current || {};
       await setProfileData(server, token, name, location, description);
     },
     setProfileImage: async (image) => {
-      let { server, token } = access.current || {};
+      const { server, token } = access.current || {};
       await setProfileImage(server, token, image);
     },
     getHandleStatus: async (name) => {
-      let { server, token } = access.current || {};
+      const { server, token } = access.current || {};
       return await getHandle(server, token, name);
     },
     setTimeFull: async (flag) => {
-      let { guid } = access.current || {};
+      const { guid } = access.current || {};
       await store.actions.setAppValue(guid, HOUR_KEY, { set: flag });
       updateState({ timeFull: flag });
     },
     setMonthLast: async (flag) => {
-      let { guid } = access.current || {};
+      const { guid } = access.current || {};
       await store.actions.setAppValue(guid, DATE_KEY, { set: flag });
       updateState({ monthLast: flag });
     },
