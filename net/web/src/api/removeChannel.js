db8ad6fdae23a1563fc13f9a0b050112e2783a36
vuong <vuongdo_777@yahoo.com>
2025-02-22 23:45:20 +0000

    Change database - fixing typo

--- net/web/src/api/removeChannel.js
+++ net/web/src/api/removeChannel.js
@@ -2,7 +2,7 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeChannel(token, channelId) {
   
-  const channel = await fetchWithTimeout(`/content/channels/${channelId}?agent=${token}`,
+  let channel = await fetchWithTimeout(`/content/channels/${channelId}?agent=${token}`,
     { method: 'DELETE' });
   checkResponse(channel);
 }
