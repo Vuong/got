2198d742a31a3f34eca3f98ae9932a6140e842f1
vuong <vuongdo_777@yahoo.com>
2025-02-22 06:46:34 +0000

    Improve styles - removing deprecated function

--- app/sdk/src/net/setCardOpenMessage.ts
+++ app/sdk/src/net/setCardOpenMessage.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { DataMessage, ContactStatus } from '../entities';
 
 export async function setCardOpenMessage(node: string, secure: boolean, message: DataMessage): Promise<ContactStatus> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/contact/openMessage`;
-  const open = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(message) });
+  let endpoint = `http${secure ? 's' : ''}://${node}/contact/openMessage`;
+  let open = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(message) });
   checkResponse(open.status);
   return await open.json();
 }
