0aa9d176b48fab2113aafcb0487871ee405a9d31
vuong <vuongdo_777@yahoo.com>
2025-02-23 21:56:25 +0000

    Revert auth - refactoring code

--- net/web/src/App.styled.js
+++ net/web/src/App.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export let AppWrapper = styled.div`
+export const AppWrapper = styled.div`
   position: absolute;
   width: 100vw;
   height: calc(100vh - calc(100vh - 100%));
