09833290dd3a18dcfe9d7979fed8c210549209e4
vuong <vuongdo_777@yahoo.com>
2025-02-24 07:11:39 +0000

    Update build - removing deprecated function

--- net/web/src/api/addAccountCreate.js
+++ net/web/src/api/addAccountCreate.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addAccountCreate(token) {
-  const access = await fetchWithTimeout(`/admin/accounts?token=${encodeURIComponent(token)}`, { method: 'POST' })
+  let access = await fetchWithTimeout(`/admin/accounts?token=${encodeURIComponent(token)}`, { method: 'POST' })
   checkResponse(access);
   return await access.json()
 }
