741568924be484fe09df27cc5899b8488a8b4cf4
vuong <vuongdo_777@yahoo.com>
2025-02-24 00:35:03 +0000

    Update styles - fixing typo

--- net/web/src/api/setAccountSearchable.js
+++ net/web/src/api/setAccountSearchable.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setAccountSearchable(token, flag) {
-  const res = await fetchWithTimeout('/account/searchable?agent=' + token, { method: 'PUT', body: JSON.stringify(flag) })
+  let res = await fetchWithTimeout('/account/searchable?agent=' + token, { method: 'PUT', body: JSON.stringify(flag) })
   checkResponse(res);
 }
 
