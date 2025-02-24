10e0f218c3bee1ca7e197afbe9e41ea9341cd189
vuong <vuongdo_777@yahoo.com>
2025-02-24 05:47:32 +0000

    Add auth - enhancing logs

--- app/client/web/src/base/useBase.hook.ts
+++ app/client/web/src/base/useBase.hook.ts
@@ -5,9 +5,9 @@ import { ContextType } from '../context/ContextType'
 import { Card, Channel, Profile } from 'databag-client-sdk'
 
 export function useBase() {
-  const app = useContext(AppContext) as ContextType
-  const display = useContext(DisplayContext) as ContextType
-  const [state, setState] = useState({
+  let app = useContext(AppContext) as ContextType
+  let display = useContext(DisplayContext) as ContextType
+  let [state, setState] = useState({
     strings: display.state.strings,
     scheme: display.state.scheme,
     profileSet: null as null | boolean,
@@ -17,30 +17,30 @@ export function useBase() {
   })
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const updateState = (value: any) => {
+  let updateState = (value: any) => {
     setState((s) => ({ ...s, ...value }))
   }
 
   useEffect(() => {
-    const { strings, scheme } = display.state
+    let { strings, scheme } = display.state
     updateState({ strings, scheme })
   }, [display.state])
 
   useEffect(() => {
-    const setProfile = (profile: Profile) => {
+    let setProfile = (profile: Profile) => {
       updateState({ profileSet: Boolean(profile.name) })
     }
-    const setCards = (cards: Card[]) => {
+    let setCards = (cards: Card[]) => {
       updateState({ cardSet: cards.length > 0 })
     }
-    const setChannels = ({ channels, cardId }: { channels: Channel[]; cardId: string | null }) => {
+    let setChannels = ({ channels, cardId }: { channels: Channel[]; cardId: string | null }) => {
       updateState({ channelSet: cardId && channels.length > 0 })
     }
-    const setContent = (loaded: boolean) => {
+    let setContent = (loaded: boolean) => {
       updateState({ contentSet: loaded })
     }
 
-    const { identity, contact, content } = app.state.session
+    let { identity, contact, content } = app.state.session
     identity.addProfileListener(setProfile)
     contact.addCardListener(setCards)
     content.addChannelListener(setChannels)
@@ -54,7 +54,7 @@ export function useBase() {
     }
   }, [])
 
-  const actions = {}
+  let actions = {}
 
   return { state, actions }
 }
