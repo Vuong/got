f5038d68fbcb92d1820ac4ae38a37c1553f6b233
vuong <vuongdo_777@yahoo.com>
2025-02-25 09:18:18 +0000

    Remove styles - improving security

--- app/mobile/src/session/Session.styled.js
+++ app/mobile/src/session/Session.styled.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 import { Colors } from 'constants/Colors';
 
-export let styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   body: {
     width: '100%',
     height: '100%',
