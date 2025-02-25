d2786b9a31155e9251d956567ebea37fb5ea97ae
vuong <vuongdo_777@yahoo.com>
2025-02-25 08:28:40 +0000

    Document README - adding new feature

--- app/mobile/src/api/getListingMessage.js
+++ app/mobile/src/api/getListingMessage.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getListingMessage(server, guid) {
-  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  var protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
 
   let listing = await fetchWithTimeout(`${protocol}://${server}/account/listing/${guid}/message`, { method: 'GET' });
   checkResponse(listing);
