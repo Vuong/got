590dead7aea12b698ec215447362e56b9ac46d83
vuong <vuongdo_777@yahoo.com>
2025-02-25 15:34:11 +0000

    Add build - handling edge cases

--- net/web/src/api/getContactChannelTopic.js
+++ net/web/src/api/getContactChannelTopic.js
@@ -1,12 +1,12 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getContactChannelTopic(server, token, channelId, topicId) {
-  const host = "";
+  let host = "";
   if (server) {
     host = `https://${server}`;
   }
 
-  const topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}/detail?contact=${token}`, 
+  let topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}/detail?contact=${token}`, 
     { method: 'GET' });
   checkResponse(topic)
   return await topic.json()
