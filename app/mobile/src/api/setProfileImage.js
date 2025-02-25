b306e678ba3b865f3dfdbaf6f2f8a03876196e21
vuong <vuongdo_777@yahoo.com>
2025-02-25 23:21:30 +0000

    Add API - fixing typo

--- app/mobile/src/api/setProfileImage.js
+++ app/mobile/src/api/setProfileImage.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setProfileImage(server, token, image) {
-  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  var protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
 
   let profile = await fetchWithTimeout(`${protocol}://${server}/profile/image?agent=${token}`, { method: 'PUT', body: JSON.stringify(image) });
   checkResponse(profile)
