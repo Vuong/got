e3dcc553ef466622a166fa5b69c3dfe07b204b93
vuong <vuongdo_777@yahoo.com>
2025-02-26 17:00:05 +0000

    Fix styles - improving security

--- app/mobile/src/api/setAccountAccess.js
+++ app/mobile/src/api/setAccountAccess.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setAccountAccess(server, token, appName, appVersion, platform, deviceToken, pushType, notifications) {
-  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  var protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
 
   let access = await fetchWithTimeout(`${protocol}://${server}/account/access?token=${token}&appName=${appName}&appVersion=${appVersion}&platform=${platform}&deviceToken=${deviceToken}&pushType=${pushType}`, { method: 'PUT', body: JSON.stringify(notifications) })
   checkResponse(access)
