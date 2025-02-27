095554766c38a5191e25b4ca1ef59524b654e061
vuong <vuongdo_777@yahoo.com>
2025-02-27 18:45:26 +0000

    Revert API - handling edge cases

--- app/sdk/src/net/getRegistryListing.ts
+++ app/sdk/src/net/getRegistryListing.ts
@@ -2,9 +2,9 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { AccountEntity } from '../entities';
 
 export async function getRegistryListing(handle: string | null, server: string, secure: boolean): Promise<AccountEntity[]> {
-  const param = handle ? `?filter=${handle}` : '';
-  const endpoint = `http${secure ? 's' : ''}://${server}/account/listing${param}`;
-  const listing = await fetchWithTimeout(endpoint, { method: 'GET' });
+  let param = handle ? `?filter=${handle}` : '';
+  let endpoint = `http${secure ? 's' : ''}://${server}/account/listing${param}`;
+  let listing = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(listing.status);
   return await listing.json();
 }
