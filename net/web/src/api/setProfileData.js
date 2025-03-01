14fc68f61db20580cc6351ef255d26bce5b9ddeb
vuong <vuongdo_777@yahoo.com>
2025-03-01 08:45:11 +0000

    Revert styles - enhancing logs

--- net/web/src/api/setProfileData.js
+++ net/web/src/api/setProfileData.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setProfileData(token, name, location, description) {
-  const data = { name: name, location: location, description: description };
-  const profile = await fetchWithTimeout(`/profile/data?agent=${token}`, { method: 'PUT', body: JSON.stringify(data) });
+  let data = { name: name, location: location, description: description };
+  let profile = await fetchWithTimeout(`/profile/data?agent=${token}`, { method: 'PUT', body: JSON.stringify(data) });
   checkResponse(profile)
   return await profile.json()
 }
