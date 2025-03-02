5f4a4feb367ab1a77b6b9bdbedfced70450228b3
vuong <vuongdo_777@yahoo.com>
2025-03-02 01:22:30 +0000

    Update styles - updating docs

--- app/sdk/src/net/addCall.ts
+++ app/sdk/src/net/addCall.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { Calling } from '../entities';
 
 export async function addCall(node: string, secure: boolean, token: string, cardId: string): Promise<Calling> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/talk/calls?agent=${token}`;
-  let call = await fetchWithTimeout(endpoint, { method: 'POST', body: JSON.stringify(cardId) });
+  const endpoint = `http${secure ? 's' : ''}://${node}/talk/calls?agent=${token}`;
+  const call = await fetchWithTimeout(endpoint, { method: 'POST', body: JSON.stringify(cardId) });
   checkResponse(call.status);
   return await call.json();
 }
