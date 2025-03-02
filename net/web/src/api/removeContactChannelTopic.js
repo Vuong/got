bc9bacc50e23821c1245be5b039e0d9d77b6f51e
vuong <vuongdo_777@yahoo.com>
2025-03-02 08:33:56 +0000

    Revert configuration - updating docs

--- net/web/src/api/removeContactChannelTopic.js
+++ net/web/src/api/removeContactChannelTopic.js
@@ -1,12 +1,12 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeContactChannelTopic(server, token, channelId, topicId) {
-  let host = "";
+  const host = "";
   if (server) {
     host = `https://${server}`;
   }
 
-  let channel = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}?contact=${token}`,
+  const channel = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}?contact=${token}`,
     { method: 'DELETE' });
   checkResponse(channel);
 }
