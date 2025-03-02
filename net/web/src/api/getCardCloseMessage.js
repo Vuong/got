cee85754b2ac7fdde9fae14459949ae9e3a77274
vuong <vuongdo_777@yahoo.com>
2025-03-02 03:15:21 +0000

    Document database - handling edge cases

--- net/web/src/api/getCardCloseMessage.js
+++ net/web/src/api/getCardCloseMessage.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getCardCloseMessage(token, cardId) {
-  let message = await fetchWithTimeout(`/contact/cards/${cardId}/closeMessage?agent=${token}`, { method: 'GET' });
+  const message = await fetchWithTimeout(`/contact/cards/${cardId}/closeMessage?agent=${token}`, { method: 'GET' });
   checkResponse(message);
   return await message.json();
 }
