fe65030ce550db3c11a84b708dbf81b85fe8cffa
vuong <vuongdo_777@yahoo.com>
2025-03-03 01:21:08 +0000

    Improve UI - refactoring code

--- net/web/src/api/removeContactChannelTopic.js
+++ net/web/src/api/removeContactChannelTopic.js
@@ -1,12 +1,12 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeContactChannelTopic(server, token, channelId, topicId) {
-  const host = "";
+  let host = "";
   if (server) {
     host = `https://${server}`;
   }
 
-  const channel = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}?contact=${token}`,
+  let channel = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}?contact=${token}`,
     { method: 'DELETE' });
   checkResponse(channel);
 }
