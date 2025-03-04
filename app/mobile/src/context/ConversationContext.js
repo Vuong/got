9abbee0f1e68afd3f120933a970cfef1c339738a
vuong <vuongdo_777@yahoo.com>
2025-03-04 19:31:05 +0000

    Optimize configuration - refactoring code

--- app/mobile/src/context/ConversationContext.js
+++ app/mobile/src/context/ConversationContext.js
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 import { useConversationContext } from './useConversationContext.hook';
 
-export const ConversationContext = createContext({});
+export let ConversationContext = createContext({});
 
 export function ConversationContextProvider({ children }) {
-  const { state, actions } = useConversationContext();
+  let { state, actions } = useConversationContext();
   return (
     <ConversationContext.Provider value={{ state, actions }}>
       {children}
