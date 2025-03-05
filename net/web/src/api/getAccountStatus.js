6f4be1607483f7e78b67ad00d4c9c31c1b497b9d
vuong <vuongdo_777@yahoo.com>
2025-03-05 10:37:47 +0000

    Refactor styles - updating docs

--- net/web/src/api/getAccountStatus.js
+++ net/web/src/api/getAccountStatus.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getAccountStatus(token) {
-  let status = await fetchWithTimeout('/account/status?agent=' + token, { method: 'GET' });
+  const status = await fetchWithTimeout('/account/status?agent=' + token, { method: 'GET' });
   checkResponse(status);
   return await status.json()
 }
