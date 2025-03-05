a2eca9ae4dc8f45228495f3513fda2a92bec586e
vuong <vuongdo_777@yahoo.com>
2025-03-05 05:31:59 +0000

    Change README - updating docs

--- net/web/src/api/addCall.js
+++ net/web/src/api/addCall.js
@@ -1,10 +1,10 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addCall(token, cardId) {
-  const param = "?agent=" + token
-  const call = await fetchWithTimeout('/talk/calls' + param, { method: 'POST', body: JSON.stringify(cardId) });
+  let param = "?agent=" + token
+  let call = await fetchWithTimeout('/talk/calls' + param, { method: 'POST', body: JSON.stringify(cardId) });
   checkResponse(call)
-  const ret = await call.json()
+  let ret = await call.json()
   return ret;
 }
 
