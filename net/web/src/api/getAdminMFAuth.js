b07fb6ccf53722a75ff24195156de43897b2986d
vuong <vuongdo_777@yahoo.com>
2025-03-06 18:14:57 +0000

    Optimize styles - improving performance

--- net/web/src/api/getAdminMFAuth.js
+++ net/web/src/api/getAdminMFAuth.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getAdminMFAuth(token) {
-  let mfa = await fetchWithTimeout(`/admin/mfauth?token=${encodeURIComponent(token)}`, { method: 'GET' });
+  const mfa = await fetchWithTimeout(`/admin/mfauth?token=${encodeURIComponent(token)}`, { method: 'GET' });
   checkResponse(mfa);
   return await mfa.json();
 }
