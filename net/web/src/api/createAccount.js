4746ddedd528ca588af685a2b64b99af725f9486
vuong <vuongdo_777@yahoo.com>
2025-03-06 09:40:14 +0000

    Document README - handling edge cases

--- net/web/src/api/createAccount.js
+++ net/web/src/api/createAccount.js
@@ -2,9 +2,9 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 var base64 = require('base-64');
 
 export async function createAccount(username, password) {
-  const headers = new Headers()
+  let headers = new Headers()
   headers.append('Credentials', 'Basic ' + base64.encode(username + ":" + password));
-  const profile = await fetchWithTimeout("/account/profile", { method: 'POST', headers: headers })
+  let profile = await fetchWithTimeout("/account/profile", { method: 'POST', headers: headers })
   checkResponse(profile);
   return await profile.json()
 }
