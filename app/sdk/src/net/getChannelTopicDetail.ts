9fcf119e5852a33cb1f3a12519d96a5170fbaef5
vuong <vuongdo_777@yahoo.com>
2025-03-08 11:00:06 +0000

    Document README - removing deprecated function

--- app/sdk/src/net/getChannelTopicDetail.ts
+++ app/sdk/src/net/getChannelTopicDetail.ts
@@ -2,10 +2,10 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { TopicDetailEntity } from '../entities';
 
 export async function getChannelTopicDetail(node: string, secure: boolean, token: string, channelId: string, topicId: string): Promise<TopicDetailEntity> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/topics/${topicId}/detail?agent=${token}`;
-  const detail = await fetchWithTimeout(endpoint, { method: 'GET' });
+  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/topics/${topicId}/detail?agent=${token}`;
+  let detail = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(detail.status);
-  const topic = await detail.json();
+  let topic = await detail.json();
   if (!topic?.data?.topicDetail) {
     throw new Error('missing topic detail');
   } else {
