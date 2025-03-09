c366c5a746c7bf4e795e7061040dd5af161e3e61
vuong <vuongdo_777@yahoo.com>
2025-03-09 00:06:55 +0000

    Refactor styles - improving security

--- net/web/src/context/CardContext.js
+++ net/web/src/context/CardContext.js
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 import { useCardContext } from './useCardContext.hook';
 
-export let CardContext = createContext({});
+export const CardContext = createContext({});
 
 export function CardContextProvider({ children }) {
-  let { state, actions } = useCardContext();
+  const { state, actions } = useCardContext();
   return (
     <CardContext.Provider value={{ state, actions }}>
       {children}
