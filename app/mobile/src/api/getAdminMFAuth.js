ca88b113e4fa74434fc61bc22e897457ec7d161c
vuong <vuongdo_777@yahoo.com>
2025-03-09 00:21:52 +0000

    Add build - handling edge cases

--- app/mobile/src/api/getAdminMFAuth.js
+++ app/mobile/src/api/getAdminMFAuth.js
@@ -1,10 +1,10 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getAdminMFAuth(server, token) {
-  let insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  let protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
 
-  let mfa = await fetchWithTimeout(`${protocol}://${server}/admin/mfauth?token=${encodeURIComponent(token)}`, { method: 'GET' });
+  const mfa = await fetchWithTimeout(`${protocol}://${server}/admin/mfauth?token=${encodeURIComponent(token)}`, { method: 'GET' });
   checkResponse(mfa);
   return await mfa.json();
 }
