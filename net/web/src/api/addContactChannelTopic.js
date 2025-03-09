b15d5f8a9930938a11ee4391d6473a3876f057b0
vuong <vuongdo_777@yahoo.com>
2025-03-09 02:31:28 +0000

    Document UI - enhancing logs

--- net/web/src/api/addContactChannelTopic.js
+++ net/web/src/api/addContactChannelTopic.js
@@ -1,45 +1,45 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addContactChannelTopic(server, token, channelId, datatype, message, assets ) {
-  var host = "";
+  let host = "";
   if (server) {
     host = `https://${server}`
   }
 
   if (message == null && (assets == null || assets.length === 0)) {
-    var topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics?contact=${token}`,
+    let topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics?contact=${token}`,
       { method: 'POST', body: JSON.stringify({}) });
     checkResponse(topic);
-    var slot = await topic.json();
+    let slot = await topic.json();
     return slot.id;
   }
   else if (assets == null || assets.length === 0) {
-    var subject = { data: JSON.stringify(message, (key, value) => {
+    let subject = { data: JSON.stringify(message, (key, value) => {
       if (value !== null) return value
     }), datatype };
 
-    var topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics?contact=${token}&confirm=true`,
+    let topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics?contact=${token}&confirm=true`,
       { method: 'POST', body: JSON.stringify(subject) });
     checkResponse(topic);
-    var slot = await topic.json();
+    let slot = await topic.json();
     return slot.id;
   }
   else {
-    var topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics?contact=${token}`,
+    let topic = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics?contact=${token}`,
       { method: 'POST', body: JSON.stringify({}) });
     checkResponse(topic);
-    var slot = await topic.json();
+    let slot = await topic.json();
 
     // add each asset
     message.assets = [];
-    for (var asset of assets) {
+    for (let asset of assets) {
       if (asset.image) {
         const formData = new FormData();
         formData.append('asset', asset.image);
-        var transform = encodeURIComponent(JSON.stringify(["ithumb;photo", "icopy;photo"]));
-        var topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
+        let transform = encodeURIComponent(JSON.stringify(["ithumb;photo", "icopy;photo"]));
+        let topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
         checkResponse(topicAsset);
-        var assetEntry = await topicAsset.json();
+        let assetEntry = await topicAsset.json();
         message.assets.push({
           image: {
             thumb: assetEntry.find(item => item.transform === 'ithumb;photo').assetId,
@@ -50,11 +50,11 @@ export async function addContactChannelTopic(server, token, channelId, datatype,
       else if (asset.video) {
         const formData = new FormData();
         formData.append('asset', asset.video);
-        var thumb = "vthumb;video;" + asset.position
-        var transform = encodeURIComponent(JSON.stringify(["vhd;video", "vlq;video", thumb]));
-        var topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
+        let thumb = "vthumb;video;" + asset.position
+        let transform = encodeURIComponent(JSON.stringify(["vhd;video", "vlq;video", thumb]));
+        let topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
         checkResponse(topicAsset);
-        var assetEntry = await topicAsset.json();
+        let assetEntry = await topicAsset.json();
         message.assets.push({
           video: {
             thumb: assetEntry.find(item => item.transform === thumb).assetId,
@@ -66,10 +66,10 @@ export async function addContactChannelTopic(server, token, channelId, datatype,
       else if (asset.audio) {
         const formData = new FormData();
         formData.append('asset', asset.audio);
-        var transform = encodeURIComponent(JSON.stringify(["acopy;audio"]));
-        var topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
+        let transform = encodeURIComponent(JSON.stringify(["acopy;audio"]));
+        let topicAsset = await fetch(`${host}/content/channels/${channelId}/topics/${slot.id}/assets?transforms=${transform}&contact=${token}`, { method: 'POST', body: formData });
         checkResponse(topicAsset);
-        var assetEntry = await topicAsset.json();
+        let assetEntry = await topicAsset.json();
         message.assets.push({
           audio: {
             label: asset.label,
@@ -79,15 +79,15 @@ export async function addContactChannelTopic(server, token, channelId, datatype,
       }
     }
 
-    var subject = { data: JSON.stringify(message, (key, value) => {
+    let subject = { data: JSON.stringify(message, (key, value) => {
       if (value !== null) return value
     }), datatype };
 
-    var unconfirmed = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${slot.id}/subject?contact=${token}`, 
+    let unconfirmed = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${slot.id}/subject?contact=${token}`, 
       { method: 'PUT', body: JSON.stringify(subject) });
     checkResponse(unconfirmed);
 
-    var confirmed = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${slot.id}/confirmed?contact=${token}`,
+    let confirmed = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${slot.id}/confirmed?contact=${token}`,
       { method: 'PUT', body: JSON.stringify('confirmed') });
     checkResponse(confirmed);
     return slot.id;
