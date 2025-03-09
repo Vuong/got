c074670c9e7eed7e624109100e8c8ded55eb63cb
vuong <vuongdo_777@yahoo.com>
2025-03-09 20:00:18 +0000

    Refactor database - fixing a bug

--- app/mobile/src/session/cardsIcon/CardsIcon.styled.js
+++ app/mobile/src/session/cardsIcon/CardsIcon.styled.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 import { Colors } from 'constants/Colors';
 
-export const styles = StyleSheet.create({
+export let styles = StyleSheet.create({
   requested: {
     width: 8,
     height: 8,
