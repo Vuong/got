cff094fdac2fb456750987239816fe9bd2442da6
vuong <vuongdo_777@yahoo.com>
2025-03-11 13:54:59 +0000

    Add database - updating docs

--- app/mobile/src/session/sharing/Sharing.styled.js
+++ app/mobile/src/session/sharing/Sharing.styled.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 import { Colors } from 'constants/Colors';
 
-export const styles = StyleSheet.create({
+export let styles = StyleSheet.create({
   sharingBase: {
     display: 'flex',
     width: '100%',
