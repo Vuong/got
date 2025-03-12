92f219ae278368b5480fc78ffbbbff1832e5d25f
vuong <vuongdo_777@yahoo.com>
2025-03-12 14:48:28 +0000

    Add build - fixing typo

--- app/mobile/src/session/useSession.hook.js
+++ app/mobile/src/session/useSession.hook.js
@@ -12,7 +12,7 @@ import { encryptChannelSubject } from 'context/sealUtil';
 
 export function useSession() {
 
-  var [state, setState] = useState({
+  const [state, setState] = useState({
     strings: getLanguageStrings(),
     tabbled: null,
     subWidth: '50%',
@@ -31,51 +31,51 @@ export function useSession() {
     remoteAudio: false,
   });
 
-  var ring = useContext(RingContext);
-  var account = useContext(AccountContext);
-  var channel = useContext(ChannelContext);
-  var card = useContext(CardContext);
-  var profile = useContext(ProfileContext);
-  var store = useContext(StoreContext);
-  var dimensions = useWindowDimensions();
-  var tabbed = useRef(null);
+  const ring = useContext(RingContext);
+  const account = useContext(AccountContext);
+  const channel = useContext(ChannelContext);
+  const card = useContext(CardContext);
+  const profile = useContext(ProfileContext);
+  const store = useContext(StoreContext);
+  const dimensions = useWindowDimensions();
+  const tabbed = useRef(null);
 
-  var updateState = (value) => {
+  const updateState = (value) => {
     setState((s) => ({ ...s, ...value }));
   }
 
   useEffect(() => {
-    var ringing = [];
-    var expired = Date.now();
+    const ringing = [];
+    const expired = Date.now();
     ring.state.ringing.forEach(call => {
       if (call.expires > expired && !call.status) {
-        var { callId, cardId, calleeToken, ice } = call;
-        var contact = card.state.cards.get(cardId);
+        const { callId, cardId, calleeToken, ice } = call;
+        const contact = card.state.cards.get(cardId);
         if (contact) {
-          var { imageSet, name, handle, node, guid } = contact.card?.profile || {};
-          var { token } = contact.card?.detail || {};
-          var contactToken = `${guid}.${token}`;
-          var server = node ? node : profile.state.server;
-          var img = imageSet ? card.actions.getCardImageUrl(cardId) : null;
+          const { imageSet, name, handle, node, guid } = contact.card?.profile || {};
+          const { token } = contact.card?.detail || {};
+          const contactToken = `${guid}.${token}`;
+          const server = node ? node : profile.state.server;
+          const img = imageSet ? card.actions.getCardImageUrl(cardId) : null;
           ringing.push({ cardId, img, name, handle, contactNode: server, callId, contactToken, calleeToken, ice });
         }
       }
     });
 
     let callLogo = null;
-    var contact = card.state.cards.get(ring.state.cardId);
+    const contact = card.state.cards.get(ring.state.cardId);
     if (contact) {
-      var { imageSet } = contact.card?.profile || {};
+      const { imageSet } = contact.card?.profile || {};
       callLogo = imageSet ? card.actions.getCardImageUrl(ring.state.cardId) : null;
     }
 
-    var { callStatus, localStream, localVideo, localAudio, remoteStream, remoteVideo, remoteAudio } = ring.state;
+    const { callStatus, localStream, localVideo, localAudio, remoteStream, remoteVideo, remoteAudio } = ring.state;
     updateState({ ringing, callStatus, callLogo, localStream, localVideo, localAudio, remoteStream, remoteVideo, remoteAudio });
   }, [ring.state]);
 
   useEffect(() => {
-    var { allowUnsealed } = account.state.status || {};
-    var { status, sealKey } = account.state;
+    const { allowUnsealed } = account.state.status || {};
+    const { status, sealKey } = account.state;
     if (status?.seal?.publicKey && sealKey?.public && sealKey?.private && sealKey?.public === status.seal.publicKey) {
       updateState({ sealable: true, allowUnsealed });
     }
@@ -89,15 +89,15 @@ export function useSession() {
     checkFirstRun();
   }, []);
 
-  var checkFirstRun = async () => {
-    var firstRun = await store.actions.getFirstRun();
+  const checkFirstRun = async () => {
+    const firstRun = await store.actions.getFirstRun();
     updateState({ firstRun });
   }
 
   useEffect(() => {
     if (tabbed.current !== true) {
       if (dimensions.width > config.tabbedWidth) {
-        var width = Math.floor((dimensions.width * 33) / 100);
+        const width = Math.floor((dimensions.width * 33) / 100);
         tabbed.current = false;
         updateState({ tabbed: false, baseWidth: width + 50, subWidth: width });
       }
@@ -108,7 +108,7 @@ export function useSession() {
     }
   }, [dimensions]);
 
-  var actions = {
+  const actions = {
     setCardId: (cardId) => {
       updateState({ cardId });
     },
@@ -120,11 +120,11 @@ export function useSession() {
       ring.actions.ignore(call.cardId, call.callId);
     },
     decline: async (call) => {
-      var { cardId, contactNode, contactToken, callId } = call;
+      const { cardId, contactNode, contactToken, callId } = call;
       await ring.actions.decline(cardId, contactNode, contactToken, callId);
     },
     accept: async (call) => {
-      var { cardId, callId, contactNode, contactToken, calleeToken, ice } = call;
+      const { cardId, callId, contactNode, contactToken, calleeToken, ice } = call;
       await ring.actions.accept(cardId, callId, contactNode, contactToken, calleeToken, ice);
     },
     end: async () => {
@@ -145,9 +145,9 @@ export function useSession() {
     setDmChannel: async (cardId) => {
       let channelId;
       channel.state.channels.forEach((entry, id) => {
-        var cards = entry?.detail?.contacts?.cards || [];
-        var subject = entry?.detail?.data || '';
-        var type = entry?.detail?.dataType || '';
+        const cards = entry?.detail?.contacts?.cards || [];
+        const subject = entry?.detail?.data || '';
+        const type = entry?.detail?.dataType || '';
         if (cards.length == 1 && cards[0] === cardId && type === 'superbasic' && subject === '{"subject":null}') {
           channelId = entry.channelId;
         }
@@ -156,14 +156,14 @@ export function useSession() {
         return channelId;
       }
       if (state.sealable && !state.allowUnsealed) {
-        var keys = [ account.state.sealKey.public ];
+        const keys = [ account.state.sealKey.public ];
         keys.push(card.state.cards.get(cardId).card.profile.seal);
-        var sealed = encryptChannelSubject(state.subject, keys);
-        var conversation = await channel.actions.addChannel('sealed', sealed, [ cardId ]);
+        const sealed = encryptChannelSubject(state.subject, keys);
+        const conversation = await channel.actions.addChannel('sealed', sealed, [ cardId ]);
         return conversation.id;
       }
       else {
-        var conversation = await channel.actions.addChannel('superbasic', { subject: null }, [ cardId ]);
+        const conversation = await channel.actions.addChannel('superbasic', { subject: null }, [ cardId ]);
         return conversation.id;
       }
     },
