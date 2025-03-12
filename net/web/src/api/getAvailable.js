52c3523021cdf0bd2863715806c29b89fab83d28
vuong <vuongdo_777@yahoo.com>
2025-03-12 07:37:49 +0000

    Add dependencies - updating docs

--- net/web/src/api/getAvailable.js
+++ net/web/src/api/getAvailable.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getAvailable() {
-  const available = await fetchWithTimeout("/account/available", { method: 'GET' })
+  let available = await fetchWithTimeout("/account/available", { method: 'GET' })
   checkResponse(available)
   return await available.json()
 }
