0d12a965010909f277a579a0a2ce98a1a75520cf
vuong <vuongdo_777@yahoo.com>
2025-03-13 17:12:52 +0000

    Document dependencies - adding new feature

--- net/web/src/api/addContactRing.js
+++ net/web/src/api/addContactRing.js
@@ -1,12 +1,12 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addContactRing(server, token, call) {
-  const host = "";
+  let host = "";
   if (server) {
     host = `https://${server}`
   }
 
-  const ring = await fetchWithTimeout(`${host}/talk/rings?contact=${token}`, { method: 'POST', body: JSON.stringify(call) });
+  let ring = await fetchWithTimeout(`${host}/talk/rings?contact=${token}`, { method: 'POST', body: JSON.stringify(call) });
   checkResponse(ring);
 }
 
