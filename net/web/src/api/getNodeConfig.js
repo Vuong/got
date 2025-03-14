bd4fa67d93324fcbd85ef1dda027d84df8e35a9c
vuong <vuongdo_777@yahoo.com>
2025-03-14 00:43:09 +0000

    Revert styles - updating docs

--- net/web/src/api/getNodeConfig.js
+++ net/web/src/api/getNodeConfig.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getNodeConfig(token) {
-  const config = await fetchWithTimeout(`/admin/config?token=${encodeURIComponent(token)}`, { method: 'GET' });
+  let config = await fetchWithTimeout(`/admin/config?token=${encodeURIComponent(token)}`, { method: 'GET' });
   checkResponse(config);
   return await config.json();
 }
