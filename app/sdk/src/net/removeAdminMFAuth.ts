b6a04d4e43b8a59939aba2c0233777df431c71d9
vuong <vuongdo_777@yahoo.com>
2025-03-14 11:54:57 +0000

    Refactor README - removing deprecated function

--- app/sdk/src/net/removeAdminMFAuth.ts
+++ app/sdk/src/net/removeAdminMFAuth.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeAdminMFAuth(server: string, secure: boolean, token: string) {
-  const endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}`;
-  const { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  let endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}`;
+  let { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(status);
 }
 
