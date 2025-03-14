3406860a4350fdfbaa874922471a0c11845b7cce
vuong <vuongdo_777@yahoo.com>
2025-03-14 16:34:03 +0000

    Revert database - adding new feature

--- net/web/src/api/getChannelTopic.js
+++ net/web/src/api/getChannelTopic.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getChannelTopic(token, channelId, topicId) {
-  const topic = await fetchWithTimeout(`/content/channels/${channelId}/topics/${topicId}/detail?agent=${token}`, 
+  let topic = await fetchWithTimeout(`/content/channels/${channelId}/topics/${topicId}/detail?agent=${token}`, 
     { method: 'GET' });
   checkResponse(topic)
   return await topic.json()
