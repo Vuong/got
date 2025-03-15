31b449aa92dde13d568001f31b487fcdf75d1ec7
vuong <vuongdo_777@yahoo.com>
2025-03-15 18:43:43 +0000

    Update API - fixing a bug

--- app/client/web/src/calling/useCalling.hook.ts
+++ app/client/web/src/calling/useCalling.hook.ts
@@ -5,10 +5,10 @@ import { ContextType } from '../context/ContextType'
 import { Card } from 'databag-client-sdk'
 
 export function useCalling() {
-  const ring = useContext(RingContext) as ContextType
-  const display = useContext(DisplayContext) as ContextType
+  let ring = useContext(RingContext) as ContextType
+  let display = useContext(DisplayContext) as ContextType
 
-  const [state, setState] = useState({
+  let [state, setState] = useState({
     strings: display.state.strings,
     calls: [] as { callId: string; card: Card }[],
     calling: null as null | Card,
@@ -23,15 +23,15 @@ export function useCalling() {
   })
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const updateState = (value: any) => {
+  let updateState = (value: any) => {
     setState((s) => ({ ...s, ...value }))
   }
 
   useEffect(() => {
-    const { calls, calling, localStream, remoteStream, localVideo, remoteVideo, audioEnabled, videoEnabled, connected, failed } = ring.state
+    let { calls, calling, localStream, remoteStream, localVideo, remoteVideo, audioEnabled, videoEnabled, connected, failed } = ring.state
     updateState({ calls, calling, localStream, remoteStream, localVideo, remoteVideo, audioEnabled, videoEnabled, connected, failed })
   }, [ring.state])
 
-  const actions = ring.actions
+  let actions = ring.actions
   return { state, actions }
 }
