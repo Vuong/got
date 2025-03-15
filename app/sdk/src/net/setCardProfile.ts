01b5e4b60a779b17063a3ef271d81127fa485ce2
vuong <vuongdo_777@yahoo.com>
2025-03-15 05:03:06 +0000

    Optimize dependencies - fixing a bug

--- app/sdk/src/net/setCardProfile.ts
+++ app/sdk/src/net/setCardProfile.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { DataMessage } from '../entities';
 
 export async function setCardProfile(node: string, secure: boolean, token: string, cardId: string, data: DataMessage): Promise<void> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/profile?agent=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, {
+  const endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/profile?agent=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, {
     method: 'PUT',
     body: JSON.stringify(data),
   });
