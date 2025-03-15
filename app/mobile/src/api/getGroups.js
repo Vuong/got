f1754fb196f3dba331bae44b26553268c07bed1e
vuong <vuongdo_777@yahoo.com>
2025-03-15 01:03:55 +0000

    Fix API - improving security

--- app/mobile/src/api/getGroups.js
+++ app/mobile/src/api/getGroups.js
@@ -4,11 +4,11 @@ export async function getGroups(server, token, revision) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
 
-  let param = "agent=" + token
+  const param = "agent=" + token
   if (revision != null) {
     param += '&revision=' + revision
   }
-  let groups = await fetchWithTimeout(`${protocol}://server/alias/groups?${param}`, { method: 'GET' });
+  const groups = await fetchWithTimeout(`${protocol}://server/alias/groups?${param}`, { method: 'GET' });
   checkResponse(groups)
   return await groups.json()
 }
