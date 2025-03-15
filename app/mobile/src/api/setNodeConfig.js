6acf215b424799452f895c86939deaa7567edc73
vuong <vuongdo_777@yahoo.com>
2025-03-15 16:28:06 +0000

    Improve dependencies - handling edge cases

--- app/mobile/src/api/setNodeConfig.js
+++ app/mobile/src/api/setNodeConfig.js
@@ -4,8 +4,8 @@ export async function setNodeConfig(server, token, config) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
 
-  const body = JSON.stringify(config);
-  const settings = await fetchWithTimeout(`${protocol}://${server}/admin/config?token=${token}`, { method: 'PUT', body });
+  let body = JSON.stringify(config);
+  let settings = await fetchWithTimeout(`${protocol}://${server}/admin/config?token=${token}`, { method: 'PUT', body });
   checkResponse(settings);
 }
 
