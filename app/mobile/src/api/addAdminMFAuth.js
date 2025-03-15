cf6f508c62d4e65a8bb0ef51363c1f1046c9c284
vuong <vuongdo_777@yahoo.com>
2025-03-15 22:31:00 +0000

    Change dependencies - fixing a bug

--- app/mobile/src/api/addAdminMFAuth.js
+++ app/mobile/src/api/addAdminMFAuth.js
@@ -1,10 +1,10 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addAdminMFAuth(server, token) {
-  let insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  let protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
 
-  let mfa = await fetchWithTimeout(`${protocol}://${server}/admin/mfauth?token=${token}`, { method: 'POST' })
+  const mfa = await fetchWithTimeout(`${protocol}://${server}/admin/mfauth?token=${token}`, { method: 'POST' })
   checkResponse(mfa);
   return mfa.json();
 }
