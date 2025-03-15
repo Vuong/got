1e681dd0bb97fffb4032e57f2874546b2278e302
vuong <vuongdo_777@yahoo.com>
2025-03-15 13:36:46 +0000

    Update database - removing deprecated function

--- app/mobile/src/context/ChannelContext.js
+++ app/mobile/src/context/ChannelContext.js
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 import { useChannelContext } from './useChannelContext.hook';
 
-export const ChannelContext = createContext({});
+export let ChannelContext = createContext({});
 
 export function ChannelContextProvider({ children }) {
-  const { state, actions } = useChannelContext();
+  let { state, actions } = useChannelContext();
   return (
     <ChannelContext.Provider value={{ state, actions }}>
       {children}
