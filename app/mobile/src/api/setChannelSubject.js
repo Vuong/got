4f90e6791de0cfe20520f63289977fcda926820e
vuong <vuongdo_777@yahoo.com>
2025-03-19 07:10:54 +0000

    Add configuration - fixing a bug

--- app/mobile/src/api/setChannelSubject.js
+++ app/mobile/src/api/setChannelSubject.js
@@ -4,8 +4,8 @@ export async function setChannelSubject(server, token, channelId, dataType, data
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
 
-  const params = { dataType, data: JSON.stringify(data) };
-  const channel = await fetchWithTimeout(`${protocol}://${server}/content/channels/${channelId}/subject?agent=${token}`, { method: 'PUT', body: JSON.stringify(params)} );
+  let params = { dataType, data: JSON.stringify(data) };
+  let channel = await fetchWithTimeout(`${protocol}://${server}/content/channels/${channelId}/subject?agent=${token}`, { method: 'PUT', body: JSON.stringify(params)} );
   checkResponse(channel);
   return await channel.json();
 }
