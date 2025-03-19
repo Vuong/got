885f6858e45843bc6497c6bff14f1c82c1f37357
vuong <vuongdo_777@yahoo.com>
2025-03-19 15:57:28 +0000

    Remove auth - improving performance

--- net/web/src/api/removeChannelTopic.js
+++ net/web/src/api/removeChannelTopic.js
@@ -2,7 +2,7 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeChannelTopic(token, channelId, topicId) {
   
-  const channel = await fetchWithTimeout(`/content/channels/${channelId}/topics/${topicId}?agent=${token}`,
+  let channel = await fetchWithTimeout(`/content/channels/${channelId}/topics/${topicId}?agent=${token}`,
     { method: 'DELETE' });
   checkResponse(channel);
 }
