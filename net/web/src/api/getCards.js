2f359c4effe16d874659af030e9178fea3161353
vuong <vuongdo_777@yahoo.com>
2025-03-20 05:41:16 +0000

    Optimize database - removing deprecated function

--- net/web/src/api/getCards.js
+++ net/web/src/api/getCards.js
@@ -1,11 +1,11 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getCards(token, revision) {
-  const param = "agent=" + token
+  let param = "agent=" + token
   if (revision != null) {
     param += '&revision=' + revision
   }
-  const cards = await fetchWithTimeout(`/contact/cards?${param}`, { method: 'GET' });
+  let cards = await fetchWithTimeout(`/contact/cards?${param}`, { method: 'GET' });
   checkResponse(cards)
   return await cards.json()
 }
