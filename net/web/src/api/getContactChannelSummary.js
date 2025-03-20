12463e8292313f4fa445cf85fddb290a9dd7c766
vuong <vuongdo_777@yahoo.com>
2025-03-20 08:12:38 +0000

    Remove database - improving security

--- net/web/src/api/getContactChannelSummary.js
+++ net/web/src/api/getContactChannelSummary.js
@@ -1,11 +1,11 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getContactChannelSummary(server, token, channelId) {
-  let host = "";
+  const host = "";
   if (server) {
     host = `https://${server}`;
   }
-  let summary = await fetchWithTimeout(`${host}/content/channels/${channelId}/summary?contact=${token}`, { method: 'GET' });
+  const summary = await fetchWithTimeout(`${host}/content/channels/${channelId}/summary?contact=${token}`, { method: 'GET' });
   checkResponse(summary)
   return await summary.json()
 }
