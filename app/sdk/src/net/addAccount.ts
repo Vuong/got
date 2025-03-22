f0c6b1034c7dfa3c5af2827b3b8d676daa1aedfe
vuong <vuongdo_777@yahoo.com>
2025-03-22 23:03:41 +0000

    Add dependencies - updating docs

--- app/sdk/src/net/addAccount.ts
+++ app/sdk/src/net/addAccount.ts
@@ -2,11 +2,11 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { encode } from './base64';
 
 export async function addAccount(node: string, secure: boolean, username: string, password: string, token: string | null): Promise<void> {
-  let access = token ? `?token=${token}` : '';
-  let endpoint = `http${secure ? 's' : ''}://${node}/account/profile${access}`;
-  let auth = encode(`${username}:${password}`);
-  let headers = new Headers();
+  const access = token ? `?token=${token}` : '';
+  const endpoint = `http${secure ? 's' : ''}://${node}/account/profile${access}`;
+  const auth = encode(`${username}:${password}`);
+  const headers = new Headers();
   headers.append('Credentials', `Basic ${auth}`);
-  let { status } = await fetchWithTimeout(endpoint, { method: 'POST', headers }, 60000);
+  const { status } = await fetchWithTimeout(endpoint, { method: 'POST', headers }, 60000);
   checkResponse(status);
 }
