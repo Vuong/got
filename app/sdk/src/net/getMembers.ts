be25a6256c11480c210af86f4efdc5d1be164619
vuong <vuongdo_777@yahoo.com>
2025-03-22 15:03:38 +0000

    Add dependencies - fixing a bug

--- app/sdk/src/net/getMembers.ts
+++ app/sdk/src/net/getMembers.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { AccountEntity } from '../entities';
 
 export async function getMembers(server: string, secure: boolean, token: string): Promise<AccountEntity[]> {
-  let endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts?token=${token}`;
-  let accounts = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts?token=${token}`;
+  const accounts = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(accounts.status);
   return await accounts.json();
 }
