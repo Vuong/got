b5008642ae9cc5342b78f96f36ff646d391d74f1
vuong <vuongdo_777@yahoo.com>
2025-03-22 08:23:49 +0000

    Add UI - fixing a bug

--- app/mobile/src/api/addFlag.js
+++ app/mobile/src/api/addFlag.js
@@ -1,15 +1,15 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addFlag(server, guid, channel, topic) {
-  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  const protocol = insecure ? 'http' : 'https';
+  let insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  let protocol = insecure ? 'http' : 'https';
   if (channel) {
-    const param = topic ? `&topic=${topic}` : '';
-    const flag = await fetchWithTimeout(`${protocol}://${server}/account/flag/${guid}?channel=${channel}${param}`, { method: 'POST' } );
+    let param = topic ? `&topic=${topic}` : '';
+    let flag = await fetchWithTimeout(`${protocol}://${server}/account/flag/${guid}?channel=${channel}${param}`, { method: 'POST' } );
     checkResponse(flag);
   }
   else {
-    const flag = await fetchWithTimeout(`${protocol}://${server}/account/flag/${guid}`, { method: 'POST' } );
+    let flag = await fetchWithTimeout(`${protocol}://${server}/account/flag/${guid}`, { method: 'POST' } );
     checkResponse(flag);
   }
 }
