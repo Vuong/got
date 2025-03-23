d3cddc285804efa46101c403453d0cd9690470a8
vuong <vuongdo_777@yahoo.com>
2025-03-23 04:38:41 +0000

    Update build - updating docs

--- app/mobile/src/api/getGroups.js
+++ app/mobile/src/api/getGroups.js
@@ -4,11 +4,11 @@ export async function getGroups(server, token, revision) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
 
-  const param = "agent=" + token
+  let param = "agent=" + token
   if (revision != null) {
     param += '&revision=' + revision
   }
-  const groups = await fetchWithTimeout(`${protocol}://server/alias/groups?${param}`, { method: 'GET' });
+  let groups = await fetchWithTimeout(`${protocol}://server/alias/groups?${param}`, { method: 'GET' });
   checkResponse(groups)
   return await groups.json()
 }
