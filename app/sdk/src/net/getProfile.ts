5059490a4aa23d8df05583e84d72c6b44d6fe68c
vuong <vuongdo_777@yahoo.com>
2025-03-24 19:35:30 +0000

    Update README - updating docs

--- app/sdk/src/net/getProfile.ts
+++ app/sdk/src/net/getProfile.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { ProfileEntity } from '../entities';
 
 export async function getProfile(node: string, secure: boolean, token: string): Promise<ProfileEntity> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/profile?agent=${token}`;
-  let profile = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/profile?agent=${token}`;
+  const profile = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(profile.status);
   return await profile.json();
 }
