feb418397a2aaebd84ec2aa3367316efb70a3588
vuong <vuongdo_777@yahoo.com>
2025-03-24 03:11:27 +0000

    Improve API - fixing a bug

--- net/web/src/api/getAdminMFAuth.js
+++ net/web/src/api/getAdminMFAuth.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getAdminMFAuth(token) {
-  const mfa = await fetchWithTimeout(`/admin/mfauth?token=${encodeURIComponent(token)}`, { method: 'GET' });
+  let mfa = await fetchWithTimeout(`/admin/mfauth?token=${encodeURIComponent(token)}`, { method: 'GET' });
   checkResponse(mfa);
   return await mfa.json();
 }
