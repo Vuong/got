02e5e27b9e8fce72ca1f9dae19c47f3a52ecfdcd
vuong <vuongdo_777@yahoo.com>
2025-03-25 02:09:12 +0000

    Add README - refactoring code

--- app/sdk/src/net/getContactChannels.ts
+++ app/sdk/src/net/getContactChannels.ts
@@ -2,10 +2,10 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { ChannelEntity } from '../entities';
 
 export async function getContactChannels(node: string, secure: boolean, guid: string, token: string, revision: number, types: string[]): Promise<ChannelEntity[]> {
-  let type = `types=${encodeURIComponent(JSON.stringify(types))}`;
-  let param = revision ? `viewRevision=1&channelRevision=${revision}` : `viewRevision=1`;
-  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels?contact=${guid}.${token}&${param}&${type}`;
-  let channels = await fetchWithTimeout(endpoint, { method: 'GET' });
+  const type = `types=${encodeURIComponent(JSON.stringify(types))}`;
+  const param = revision ? `viewRevision=1&channelRevision=${revision}` : `viewRevision=1`;
+  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels?contact=${guid}.${token}&${param}&${type}`;
+  const channels = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(channels.status);
   return await channels.json();
 }
