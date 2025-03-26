2640bfe54f1e10a7592f276d4132a1d0c996980c
vuong <vuongdo_777@yahoo.com>
2025-03-26 04:17:37 +0000

    Revert tests - removing deprecated function

--- net/web/src/api/addCard.js
+++ net/web/src/api/addCard.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addCard(token, message) {
-  const card = await fetchWithTimeout(`/contact/cards?agent=${token}`, { method: 'POST', body: JSON.stringify(message)} );
+  let card = await fetchWithTimeout(`/contact/cards?agent=${token}`, { method: 'POST', body: JSON.stringify(message)} );
   checkResponse(card);
   return await card.json();
 }
