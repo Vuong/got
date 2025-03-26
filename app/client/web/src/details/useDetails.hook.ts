90512e5d6d2fadffb0b09e32a0d983412150de93
vuong <vuongdo_777@yahoo.com>
2025-03-26 19:35:47 +0000

    Document configuration - refactoring code

--- app/client/web/src/details/useDetails.hook.ts
+++ app/client/web/src/details/useDetails.hook.ts
@@ -5,9 +5,9 @@ import { ContextType } from '../context/ContextType'
 import { FocusDetail, Card, Profile } from 'databag-client-sdk'
 
 export function useDetails() {
-  let display = useContext(DisplayContext) as ContextType
-  let app = useContext(AppContext) as ContextType
-  let [state, setState] = useState({
+  const display = useContext(DisplayContext) as ContextType
+  const app = useContext(AppContext) as ContextType
+  const [state, setState] = useState({
     cardId: null as null | string,
     channelId: '',
     detail: undefined as undefined | FocusDetail,
@@ -29,14 +29,14 @@ export function useDetails() {
   })
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let updateState = (value: any) => {
+  const updateState = (value: any) => {
     setState((s) => ({ ...s, ...value }))
   }
 
-  let getTimestamp = (created: number) => {
-    let now = Math.floor(new Date().getTime() / 1000)
-    let date = new Date(created * 1000)
-    let offset = now - created
+  const getTimestamp = (created: number) => {
+    const now = Math.floor(new Date().getTime() / 1000)
+    const date = new Date(created * 1000)
+    const offset = now - created
     if (offset < 43200) {
       if (state.timeFormat === '12h') {
         return date.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' })
@@ -59,28 +59,28 @@ export function useDetails() {
   }
 
   useEffect(() => {
-    let { strings, timeFormat, dateFormat } = display.state
+    const { strings, timeFormat, dateFormat } = display.state
     updateState({ strings, timeFormat, dateFormat })
   }, [display.state])
 
   useEffect(() => {
     console.log('DETAILS', state.detail)
 
-    let hostCard = state.cards.find((entry) => entry.cardId == state.cardId)
-    let profileRemoved = state.detail?.members ? state.detail.members.filter((member) => state.profile?.guid != member.guid) : []
-    let contactCards = profileRemoved.map((member) => state.cards.find((card) => card.guid === member.guid))
-    let channelCards = contactCards.filter((member) => Boolean(member))
-    let unknownContacts = contactCards.length - channelCards.length
+    const hostCard = state.cards.find((entry) => entry.cardId == state.cardId)
+    const profileRemoved = state.detail?.members ? state.detail.members.filter((member) => state.profile?.guid != member.guid) : []
+    const contactCards = profileRemoved.map((member) => state.cards.find((card) => card.guid === member.guid))
+    const channelCards = contactCards.filter((member) => Boolean(member))
+    const unknownContacts = contactCards.length - channelCards.length
     updateState({ hostCard, channelCards, unknownContacts })
   }, [state.detail, state.cards, state.profile, state.cardId])
 
   useEffect(() => {
-    let focus = app.state.focus
-    let { contact, identity } = app.state.session || {}
+    const focus = app.state.focus
+    const { contact, identity } = app.state.session || {}
     if (focus && contact && identity) {
-      let setCards = (cards: Card[]) => {
-        let filtered = cards.filter((card) => !card.blocked)
-        let sorted = filtered.sort((a, b) => {
+      const setCards = (cards: Card[]) => {
+        const filtered = cards.filter((card) => !card.blocked)
+        const sorted = filtered.sort((a, b) => {
           if (a.handle > b.handle) {
             return 1
           } else if (a.handle < b.handle) {
@@ -91,19 +91,19 @@ export function useDetails() {
         })
         updateState({ cards: sorted })
       }
-      let setProfile = (profile: Profile) => {
+      const setProfile = (profile: Profile) => {
         updateState({ profile })
       }
-      let setDetail = (focused: { cardId: string | null; channelId: string; detail: FocusDetail | null }) => {
-        let detail = focused ? focused.detail : null
-        let cardId = focused.cardId
-        let channelId = focused.channelId
-        let access = Boolean(detail)
-        let sealed = detail?.sealed
-        let locked = detail?.locked
-        let host = cardId == null
-        let subject = detail?.data?.subject ? detail.data.subject : ''
-        let created = detail?.created ? getTimestamp(detail.created) : ''
+      const setDetail = (focused: { cardId: string | null; channelId: string; detail: FocusDetail | null }) => {
+        const detail = focused ? focused.detail : null
+        const cardId = focused.cardId
+        const channelId = focused.channelId
+        const access = Boolean(detail)
+        const sealed = detail?.sealed
+        const locked = detail?.locked
+        const host = cardId == null
+        const subject = detail?.data?.subject ? detail.data.subject : ''
+        const created = detail?.created ? getTimestamp(detail.created) : ''
         updateState({ detail, editSubject: subject, subject, channelId, cardId, access, sealed, locked, host, created })
       }
       focus.addDetailListener(setDetail)
@@ -117,32 +117,32 @@ export function useDetails() {
     }
   }, [app.state.focus, state.timeFormat, state.dateFormat])
 
-  let actions = {
+  const actions = {
     remove: async () => {
-      let content = app.state.session.getContent()
+      const content = app.state.session.getContent()
       await content.removeChannel(state.channelId)
       app.actions.clearFocus()
     },
     leave: async () => {
-      let content = app.state.session.getContent()
+      const content = app.state.session.getContent()
       await content.leaveChannel(state.cardId, state.channelId)
       app.actions.clearFocus()
     },
     block: async () => {
-      let content = app.state.session.getContent()
+      const content = app.state.session.getContent()
       await content.setBlockedChannel(state.cardId, state.channelId, true)
       app.actions.clearFocus()
     },
     report: async () => {
-      let content = app.state.session.getContent()
+      const content = app.state.session.getContent()
       await content.flagChannel(state.cardId, state.channelId)
     },
     setMember: async (cardId: string) => {
-      let content = app.state.session.getContent()
+      const content = app.state.session.getContent()
       await content.setChannelCard(state.channelId, cardId)
     },
     clearMember: async (cardId: string) => {
-      let content = app.state.session.getContent()
+      const content = app.state.session.getContent()
       await content.clearChannelCard(state.channelId, cardId)
     },
     setEditSubject: (editSubject: string) => {
@@ -152,7 +152,7 @@ export function useDetails() {
       updateState({ editSubject: state.subject })
     },
     saveSubject: async () => {
-      let content = app.state.session.getContent()
+      const content = app.state.session.getContent()
       await content.setChannelSubject(state.channelId, state.sealed ? 'sealed' : 'superbasic', { subject: state.editSubject })
     },
   }
