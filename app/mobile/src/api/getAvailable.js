1a5fb71a4c4eab85d7cb2b8a59a7f2e4982effb1
vuong <vuongdo_777@yahoo.com>
2025-03-27 01:02:49 +0000

    Remove UI - removing deprecated function

--- app/mobile/src/api/getAvailable.js
+++ app/mobile/src/api/getAvailable.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getAvailable(server) {
-  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  const protocol = insecure ? 'http' : 'https';
+  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  var protocol = insecure ? 'http' : 'https';
   let available = await fetchWithTimeout(`${protocol}://${server}/account/available`, { method: 'GET' })
   checkResponse(available)
   return await available.json()
