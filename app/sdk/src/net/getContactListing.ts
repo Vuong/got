f18ecb5dbaf36185d39e310f55c4b86e20873a97
vuong <vuongdo_777@yahoo.com>
2025-03-27 16:23:03 +0000

    Document API - removing deprecated function

--- app/sdk/src/net/getContactListing.ts
+++ app/sdk/src/net/getContactListing.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { DataMessage } from '../entities';
 
 export async function getContactListing(node: string, secure: boolean, guid: string): Promise<DataMessage> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/account/listing/${guid}/message`;
-  const listing = await fetchWithTimeout(endpoint, { method: 'GET' });
+  let endpoint = `http${secure ? 's' : ''}://${node}/account/listing/${guid}/message`;
+  let listing = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(listing.status);
   return await listing.json();
 }
