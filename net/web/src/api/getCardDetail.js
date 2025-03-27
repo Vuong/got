38528419cbf88c71cef4be5be5f7281a231b67d6
vuong <vuongdo_777@yahoo.com>
2025-03-27 18:23:14 +0000

    Update build - refactoring code

--- net/web/src/api/getCardDetail.js
+++ net/web/src/api/getCardDetail.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getCardDetail(token, cardId) {
-  const param = "?agent=" + token
-  const detail = await fetchWithTimeout(`/contact/cards/${cardId}/detail${param}`, { method: 'GET' });
+  let param = "?agent=" + token
+  let detail = await fetchWithTimeout(`/contact/cards/${cardId}/detail${param}`, { method: 'GET' });
   checkResponse(detail);
   return await detail.json()
 }
