22f0589785d8af26bf520fd5285234a432901a74
vuong <vuongdo_777@yahoo.com>
2025-03-29 14:09:45 +0000

    Add database - improving performance

--- app/mobile/src/api/getAccountImageUrl.js
+++ app/mobile/src/api/getAccountImageUrl.js
@@ -1,6 +1,6 @@
 export function getAccountImageUrl(server, token, accountId) {
-  let insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  let protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
   return `${protocol}://${server}/admin/accounts/${accountId}/image?token=${token}`
 }
 
