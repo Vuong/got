1eab3029f7b43e5a6df71bae5adb079365bd2b88
vuong <vuongdo_777@yahoo.com>
2025-03-30 01:00:09 +0000

    Revert build - fixing a bug

--- app/client/mobile/src/identity/Identity.styled.ts
+++ app/client/mobile/src/identity/Identity.styled.ts
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
-export let styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   identity: {
     display: 'flex',
     flexDirection: 'row',
