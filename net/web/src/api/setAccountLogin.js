abc0d4299df794f65bc9238e946bf91b25f93768
vuong <vuongdo_777@yahoo.com>
2025-03-30 09:32:18 +0000

    Update auth - improving security

--- net/web/src/api/setAccountLogin.js
+++ net/web/src/api/setAccountLogin.js
@@ -2,9 +2,9 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 var base64 = require('base-64');
 
 export async function setAccountLogin(token, username, password) {
-  const headers = new Headers()
+  let headers = new Headers()
   headers.append('Credentials', 'Basic ' + base64.encode(username + ":" + password));
-  const res = await fetchWithTimeout(`/account/login?agent=${token}`, { method: 'PUT', headers })
+  let res = await fetchWithTimeout(`/account/login?agent=${token}`, { method: 'PUT', headers })
   checkResponse(res);
 }
 
