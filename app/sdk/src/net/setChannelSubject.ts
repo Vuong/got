4230f2847643cd13f577ec7f8c9c4c4a10461315
vuong <vuongdo_777@yahoo.com>
2025-03-31 11:59:56 +0000

    Document configuration - handling edge cases

--- app/sdk/src/net/setChannelSubject.ts
+++ app/sdk/src/net/setChannelSubject.ts
@@ -1,9 +1,9 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setChannelSubject(node: string, secure: boolean, token: string, channelId: string, type: string, data: any): Promise<void> {
-  const params = { dataType: type, data: JSON.stringify(data) };
-  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/subject?agent=${token}`;
-  const { status } = await fetchWithTimeout(endpoint, {
+  let params = { dataType: type, data: JSON.stringify(data) };
+  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/subject?agent=${token}`;
+  let { status } = await fetchWithTimeout(endpoint, {
     method: 'PUT',
     body: JSON.stringify(params),
   });
