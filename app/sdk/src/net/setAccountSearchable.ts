ff92ecaeadd1bde14766e279befc911c75f44231
vuong <vuongdo_777@yahoo.com>
2025-03-31 23:32:07 +0000

    Add API - fixing typo

--- app/sdk/src/net/setAccountSearchable.ts
+++ app/sdk/src/net/setAccountSearchable.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setAccountSearchable(node: string, secure: boolean, token: string, flag: boolean) {
-  let endpoint = `http${secure ? 's' : ''}://${node}/account/searchable?agent=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, {
+  const endpoint = `http${secure ? 's' : ''}://${node}/account/searchable?agent=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, {
     method: 'PUT',
     body: JSON.stringify(flag),
   });
