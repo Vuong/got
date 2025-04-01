a39990b6bef288eccba06a308e8750ce3f604dc3
vuong <vuongdo_777@yahoo.com>
2025-04-01 07:00:41 +0000

    Improve dependencies - handling edge cases

--- app/sdk/src/net/clearChannelCard.ts
+++ app/sdk/src/net/clearChannelCard.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function clearChannelCard(node: string, secure: boolean, token: string, channelId: string, cardId: string): Promise<void> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/cards/${cardId}?agent=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/cards/${cardId}?agent=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(status);
 }
