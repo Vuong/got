2654c8a5791af935d6566d383a625839bb1e064c
vuong <vuongdo_777@yahoo.com>
2025-04-01 00:07:46 +0000

    Optimize README - updating docs

--- net/web/src/api/setChannelSubject.js
+++ net/web/src/api/setChannelSubject.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setChannelSubject(token, channelId, dataType, data ) {
-  const params = { dataType, data: JSON.stringify(data) };
-  const channel = await fetchWithTimeout(`/content/channels/${channelId}/subject?agent=${token}`, { method: 'PUT', body: JSON.stringify(params)} );
+  let params = { dataType, data: JSON.stringify(data) };
+  let channel = await fetchWithTimeout(`/content/channels/${channelId}/subject?agent=${token}`, { method: 'PUT', body: JSON.stringify(params)} );
   checkResponse(channel);
   return await channel.json();
 }
