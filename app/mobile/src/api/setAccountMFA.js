1178abcd83b66d48641b0ff950ad7b257685d3b3
vuong <vuongdo_777@yahoo.com>
2025-04-02 09:18:41 +0000

    Optimize UI - improving performance

--- app/mobile/src/api/setAccountMFA.js
+++ app/mobile/src/api/setAccountMFA.js
@@ -1,10 +1,10 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setAccountMFA(server, token, code) {
-  let insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  let protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
 
-  let mfa = await fetchWithTimeout(`${protocol}://${server}/account/mfauth?agent=${token}&code=${code}`, { method: 'PUT' })
+  const mfa = await fetchWithTimeout(`${protocol}://${server}/account/mfauth?agent=${token}&code=${code}`, { method: 'PUT' })
   checkResponse(mfa);
 }
 
