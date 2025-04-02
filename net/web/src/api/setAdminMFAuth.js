29d49dbbc8d73e95624264721b9b84936fd67d68
vuong <vuongdo_777@yahoo.com>
2025-04-02 21:10:18 +0000

    Improve configuration - handling edge cases

--- net/web/src/api/setAdminMFAuth.js
+++ net/web/src/api/setAdminMFAuth.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setAdminMFAuth(token, code) {
-  let mfa = await fetchWithTimeout(`/admin/mfauth?token=${token}&code=${code}`, { method: 'PUT' })
+  const mfa = await fetchWithTimeout(`/admin/mfauth?token=${token}&code=${code}`, { method: 'PUT' })
   checkResponse(mfa);
 }
 
