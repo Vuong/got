8c7e6a59c074be3b69a2a0318c33448a93597eb1
vuong <vuongdo_777@yahoo.com>
2025-04-03 18:42:15 +0000

    Add configuration - refactoring code

--- net/web/src/api/setContactChannelTopicSubject.js
+++ net/web/src/api/setContactChannelTopicSubject.js
@@ -1,16 +1,16 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setContactChannelTopicSubject(server, token, channelId, topicId, datatype, data) {
-  const host = "";
+  let host = "";
   if (server) {
     host = `https://${server}`;
   }
 
-  const subject = { data: JSON.stringify(data, (key, value) => {
+  let subject = { data: JSON.stringify(data, (key, value) => {
     if (value !== null) return value
   }), datatype };
 
-  const channel = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}/subject?contact=${token}&confirm=true`,
+  let channel = await fetchWithTimeout(`${host}/content/channels/${channelId}/topics/${topicId}/subject?contact=${token}&confirm=true`,
     { method: 'PUT', body: JSON.stringify(subject) });
   checkResponse(channel);
 }
