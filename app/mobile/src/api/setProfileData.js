f675abb0b25647ad80690eb31631e322111dceb0
vuong <vuongdo_777@yahoo.com>
2025-04-03 14:41:58 +0000

    Refactor styles - fixing a bug

--- app/mobile/src/api/setProfileData.js
+++ app/mobile/src/api/setProfileData.js
@@ -4,8 +4,8 @@ export async function setProfileData(server, token, name, location, description)
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
 
-  let data = { name: name, location: location, description: description };
-  let profile = await fetchWithTimeout(`${protocol}://${server}/profile/data?agent=${token}`, { method: 'PUT', body: JSON.stringify(data) });
+  const data = { name: name, location: location, description: description };
+  const profile = await fetchWithTimeout(`${protocol}://${server}/profile/data?agent=${token}`, { method: 'PUT', body: JSON.stringify(data) });
   checkResponse(profile)
   return await profile.json()
 }
