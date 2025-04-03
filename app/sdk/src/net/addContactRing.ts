bd39d6deb26e5eccd7b0ff504242320796ae2aa9
vuong <vuongdo_777@yahoo.com>
2025-04-03 19:21:31 +0000

    Refactor auth - improving performance

--- app/sdk/src/net/addContactRing.ts
+++ app/sdk/src/net/addContactRing.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { Ringing } from '../entities';
 
 export async function addContactRing(server: string, secure: boolean, guid: string, token: string, ringing: Ringing) {
-  const endpoint = `http${secure ? 's' : '' }://${server}/talk/rings?contact=${guid}.${token}`;
-  const { status } = await fetchWithTimeout(endpoint, { method: 'POST', body: JSON.stringify(ringing) });
+  let endpoint = `http${secure ? 's' : '' }://${server}/talk/rings?contact=${guid}.${token}`;
+  let { status } = await fetchWithTimeout(endpoint, { method: 'POST', body: JSON.stringify(ringing) });
   checkResponse(status);
 }
 
