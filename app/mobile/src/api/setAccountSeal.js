b85cac6f4062002aa976e1fe3dacb6021e7a4006
vuong <vuongdo_777@yahoo.com>
2025-04-04 05:40:07 +0000

    Revert README - improving security

--- app/mobile/src/api/setAccountSeal.js
+++ app/mobile/src/api/setAccountSeal.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setAccountSeal(server, token, seal) {
-  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  var protocol = insecure ? 'http' : 'https';
+  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  const protocol = insecure ? 'http' : 'https';
 
   let res = await fetchWithTimeout(`${protocol}://${server}/account/seal?agent=${token}`, { method: 'PUT', body: JSON.stringify(seal) })
   checkResponse(res);
