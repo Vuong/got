e18312d21779151f70f5149f366e539039c9241c
vuong <vuongdo_777@yahoo.com>
2025-04-05 08:24:57 +0000

    Fix API - refactoring code

--- net/web/src/api/getCardProfile.js
+++ net/web/src/api/getCardProfile.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getCardProfile(token, cardId) {
-  let profile = await fetchWithTimeout(`/contact/cards/${cardId}/profile?agent=${token}`, { method: 'GET' });
+  const profile = await fetchWithTimeout(`/contact/cards/${cardId}/profile?agent=${token}`, { method: 'GET' });
   checkResponse(profile);
   return await profile.json()
 }
