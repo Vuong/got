9d9745395afaad9b313db497f645ac4b861c4976
vuong <vuongdo_777@yahoo.com>
2025-04-06 14:25:29 +0000

    Change API - updating docs

--- net/web/src/session/identity/useIdentity.hook.js
+++ net/web/src/session/identity/useIdentity.hook.js
@@ -5,7 +5,7 @@ import { SettingsContext } from 'context/SettingsContext';
 
 export function useIdentity() {
 
-  var [state, setState] = useState({
+  const [state, setState] = useState({
     url: null,
     name: null,
     handle: null,
@@ -16,33 +16,33 @@ export function useIdentity() {
     menuStyle: {},
   });
 
-  var app = useContext(AppContext);
-  var profile = useContext(ProfileContext);
-  var settings = useContext(SettingsContext);
+  const app = useContext(AppContext);
+  const profile = useContext(ProfileContext);
+  const settings = useContext(SettingsContext);
 
-  var updateState = (value) => {
+  const updateState = (value) => {
     setState((s) => ({ ...s, ...value }));
   }
 
   useEffect(() => {
     if (profile.state.identity?.guid) {
-      var { name, handle, image } = profile.state.identity;
+      const { name, handle, image } = profile.state.identity;
       let url = !image ? null : profile.state.imageUrl;
       updateState({ init: true, name, handle, url });
     }
   }, [profile.state]);
 
   useEffect(() => {
-    var { status } = app.state;
+    const { status } = app.state;
     updateState({ status });
   }, [app.state]);
 
   useEffect(() => {
-    var { colors, strings, menuStyle } = settings.state;
+    const { colors, strings, menuStyle } = settings.state;
     updateState({ colors, strings, menuStyle });
   }, [settings.state]);
 
-  var actions = {
+  const actions = {
     logout: (all) => {
       app.actions.logout(all);
     },
