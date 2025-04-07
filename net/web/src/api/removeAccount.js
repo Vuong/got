5ca13e0f19feaaa694a362f6061a46c5248007e0
vuong <vuongdo_777@yahoo.com>
2025-04-07 04:31:09 +0000

    Improve README - refactoring code

--- net/web/src/api/removeAccount.js
+++ net/web/src/api/removeAccount.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeAccount(token, accountId) {
-  const res = await fetchWithTimeout(`/admin/accounts/${accountId}?token=${encodeURIComponent(token)}`, { method: 'DELETE' })
+  let res = await fetchWithTimeout(`/admin/accounts/${accountId}?token=${encodeURIComponent(token)}`, { method: 'DELETE' })
   checkResponse(res);
 }
 
