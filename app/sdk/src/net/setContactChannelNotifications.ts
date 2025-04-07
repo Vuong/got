224e6314b1030dc94f87036d5da6bc6169212a00
vuong <vuongdo_777@yahoo.com>
2025-04-07 10:20:02 +0000

    Improve tests - updating docs

--- app/sdk/src/net/setContactChannelNotifications.ts
+++ app/sdk/src/net/setContactChannelNotifications.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setContactChannelNotifications(node: string, secure: boolean, guid: string, token: string, channelId: string, enabled: boolean) {
-  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/notification?contact=${guid}.${token}`;
-  let notify = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(enabled) });
+  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/notification?contact=${guid}.${token}`;
+  const notify = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(enabled) });
   checkResponse(notify.status);
 }
