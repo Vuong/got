d46389d7ddbf8ca324c32fffae0a3120a0729bc4
vuong <vuongdo_777@yahoo.com>
2025-04-08 14:35:05 +0000

    Refactor build - enhancing logs

--- app/mobile/src/api/getCard.js
+++ app/mobile/src/api/getCard.js
@@ -3,8 +3,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 export async function getCard(server, token, cardId) {
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
-  let param = "?agent=" + token
-  let card = await fetchWithTimeout(`${protocol}://${server}/contact/cards/${cardId}${param}`, { method: 'GET' });
+  const param = "?agent=" + token
+  const card = await fetchWithTimeout(`${protocol}://${server}/contact/cards/${cardId}${param}`, { method: 'GET' });
   checkResponse(card);
   return await card.json()
 }
