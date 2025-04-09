cf91005ee78f3f3d933dbc70279390c32b844585
vuong <vuongdo_777@yahoo.com>
2025-04-09 19:20:36 +0000

    Remove README - updating docs

--- net/web/src/api/clearChannelCard.js
+++ net/web/src/api/clearChannelCard.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function clearChannelCard(token, channelId, cardId ) {
-  const channel = await fetchWithTimeout(`/content/channels/${channelId}/cards/${cardId}?agent=${token}`, {method: 'DELETE'});
+  let channel = await fetchWithTimeout(`/content/channels/${channelId}/cards/${cardId}?agent=${token}`, {method: 'DELETE'});
   checkResponse(channel);
   return await channel.json();
 }
