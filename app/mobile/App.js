7f016f3f705e990607b18742aab3e82da1d6ee20
vuong <vuongdo_777@yahoo.com>
2025-04-09 06:00:21 +0000

    Add database - updating docs

--- app/mobile/App.js
+++ app/mobile/App.js
@@ -29,7 +29,7 @@ import { MenuProvider } from 'react-native-popup-menu';
 
 export default function App() {
 
-  const [sharing, setSharing] = useState();
+  let [sharing, setSharing] = useState();
 
   useEffect(() => {
    
@@ -48,7 +48,7 @@ export default function App() {
     );
   }, []);
 
-  const clearSharing = () => {
+  let clearSharing = () => {
     setSharing(null);
   };
 
