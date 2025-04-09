9ab7c2ea2d41bdb55b6930e47a919ee0bedc6b74
vuong <vuongdo_777@yahoo.com>
2025-04-09 19:44:21 +0000

    Improve dependencies - fixing typo

--- app/sdk/src/net/setNodeAccount.ts
+++ app/sdk/src/net/setNodeAccount.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setNodeAccount(server: string, secure: boolean, token: string, accountId: number, disabled: boolean) {
-  const endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts/${accountId}/status?token=${token}`;
-  const { status } = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(disabled) });
+  let endpoint = `http${secure ? 's' : ''}://${server}/admin/accounts/${accountId}/status?token=${token}`;
+  let { status } = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(disabled) });
   checkResponse(status);
 }
