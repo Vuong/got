31f607f5fec3ed446ae615a84a6b277be5b1be01
vuong <vuongdo_777@yahoo.com>
2025-04-10 02:09:37 +0000

    Improve UI - handling edge cases

--- app/sdk/src/net/getContactProfile.ts
+++ app/sdk/src/net/getContactProfile.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { DataMessage } from '../entities';
 
 export async function getContactProfile(node: string, secure: boolean, guid: string, token: string): Promise<DataMessage> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/profile/message?contact=${guid}.${token}`;
-  const profile = await fetchWithTimeout(endpoint, { method: 'GET' });
+  let endpoint = `http${secure ? 's' : ''}://${node}/profile/message?contact=${guid}.${token}`;
+  let profile = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(profile.status);
   return await profile.json();
 }
