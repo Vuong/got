9faccf9f147ff5bd6602f964b83e6a9657e4e384
vuong <vuongdo_777@yahoo.com>
2025-04-10 06:34:24 +0000

    Document dependencies - handling edge cases

--- app/sdk/src/net/setChannelCard.ts
+++ app/sdk/src/net/setChannelCard.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setChannelCard(node: string, secure: boolean, token: string, channelId: string, cardId: string): Promise<void> {
-  let endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/cards/${cardId}?agent=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, { method: 'PUT' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/content/channels/${channelId}/cards/${cardId}?agent=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, { method: 'PUT' });
   checkResponse(status);
 }
