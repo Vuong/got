1fc6cb84906a41e9d720b94aa425e73eae4d7f34
vuong <vuongdo_777@yahoo.com>
2025-04-10 18:36:42 +0000

    Change dependencies - handling edge cases

--- app/sdk/src/net/getCardOpenMessage.ts
+++ app/sdk/src/net/getCardOpenMessage.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { DataMessage } from '../entities';
 
 export async function getCardOpenMessage(node: string, secure: boolean, token: string, cardId: string): Promise<DataMessage> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/openMessage?agent=${token}`;
-  const open = await fetchWithTimeout(endpoint, { method: 'GET' });
+  let endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/openMessage?agent=${token}`;
+  let open = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(open.status);
   return await open.json();
 }
