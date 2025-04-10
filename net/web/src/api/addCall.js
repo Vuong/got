c8677b698a303258158a7bca1f0e47f98d2db998
vuong <vuongdo_777@yahoo.com>
2025-04-10 13:31:52 +0000

    Document dependencies - handling edge cases

--- net/web/src/api/addCall.js
+++ net/web/src/api/addCall.js
@@ -1,10 +1,10 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addCall(token, cardId) {
-  let param = "?agent=" + token
-  let call = await fetchWithTimeout('/talk/calls' + param, { method: 'POST', body: JSON.stringify(cardId) });
+  const param = "?agent=" + token
+  const call = await fetchWithTimeout('/talk/calls' + param, { method: 'POST', body: JSON.stringify(cardId) });
   checkResponse(call)
-  let ret = await call.json()
+  const ret = await call.json()
   return ret;
 }
 
