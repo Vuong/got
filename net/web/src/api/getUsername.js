b5c093c0f045aebfb11bc242750085116e54c345
vuong <vuongdo_777@yahoo.com>
2025-04-10 03:32:59 +0000

    Refactor dependencies - fixing typo

--- net/web/src/api/getUsername.js
+++ net/web/src/api/getUsername.js
@@ -1,11 +1,11 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getUsername(name, token) {
-  const access = "";
+  let access = "";
   if (token) {
     access = `&token=${token}`
   }
-  const available = await fetchWithTimeout('/account/username?name=' + encodeURIComponent(name) + access, { method: 'GET' })
+  let available = await fetchWithTimeout('/account/username?name=' + encodeURIComponent(name) + access, { method: 'GET' })
   checkResponse(available)
   return await available.json()
 }
