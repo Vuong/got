2a9d5cf87dfab1b44878e0b16ac5bf73a37b4300
vuong <vuongdo_777@yahoo.com>
2025-04-11 12:28:15 +0000

    Fix build - enhancing logs

--- net/web/src/api/getContactChannels.js
+++ net/web/src/api/getContactChannels.js
@@ -1,21 +1,21 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getContactChannels(server, token, viewRevision, channelRevision) {
-  const host = "";
+  let host = "";
   if (server) {
     host = `https://${server}`;
   }
 
-  const param = "?contact=" + token
+  let param = "?contact=" + token
   if (viewRevision != null) {
     param += '&viewRevision=' + viewRevision
   }
   if (channelRevision != null) {
     param += '&channelRevision=' + channelRevision
   }
-  const types = encodeURIComponent(JSON.stringify([ 'sealed', 'superbasic' ]));
+  let types = encodeURIComponent(JSON.stringify([ 'sealed', 'superbasic' ]));
   param += `&types=${types}`
-  const channels = await fetchWithTimeout(`${host}/content/channels${param}`, { method: 'GET' });
+  let channels = await fetchWithTimeout(`${host}/content/channels${param}`, { method: 'GET' });
   checkResponse(channels)
   return await channels.json()
 }
