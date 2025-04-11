53375eac0d0884522ac28a8cd027ee66652a6a4a
vuong <vuongdo_777@yahoo.com>
2025-04-11 01:26:08 +0000

    Update styles - fixing typo

--- net/web/src/session/bottomNav/BottomNav.styled.js
+++ net/web/src/session/bottomNav/BottomNav.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Colors } from 'constants/Colors';
 
-export let BottomNavWrapper = styled.div`
+export const BottomNavWrapper = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
