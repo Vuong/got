b70963d12c14e043a7055c4c345420b4de5fe745
vuong <vuongdo_777@yahoo.com>
2025-04-12 19:40:59 +0000

    Update database - refactoring code

--- net/web/src/api/getListingMessage.js
+++ net/web/src/api/getListingMessage.js
@@ -1,12 +1,12 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getListingMessage(server, guid) {
-  const host = "";
+  let host = "";
   if (server) {
     host = `https://${server}`;
   }
 
-  const listing = await fetchWithTimeout(`${host}/account/listing/${guid}/message`, { method: 'GET' });
+  let listing = await fetchWithTimeout(`${host}/account/listing/${guid}/message`, { method: 'GET' });
   checkResponse(listing);
   return await listing.json();
 }
