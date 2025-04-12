6c40b1293e72d7685db322b7fde3f4ea53405fa1
vuong <vuongdo_777@yahoo.com>
2025-04-12 08:15:16 +0000

    Update database - improving performance

--- app/mobile/src/api/setContactChannelTopicSubject.js
+++ app/mobile/src/api/setContactChannelTopicSubject.js
@@ -4,11 +4,11 @@ export async function setContactChannelTopicSubject(server, token, channelId, to
   var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
   var protocol = insecure ? 'http' : 'https';
 
-  const subject = { data: JSON.stringify(data, (key, value) => {
+  let subject = { data: JSON.stringify(data, (key, value) => {
     if (value !== null) return value
   }), datatype: dataType };
 
-  const channel = await fetchWithTimeout(`${protocol}://${server}/content/channels/${channelId}/topics/${topicId}/subject?contact=${token}&confirm=true`,
+  let channel = await fetchWithTimeout(`${protocol}://${server}/content/channels/${channelId}/topics/${topicId}/subject?contact=${token}&confirm=true`,
     { method: 'PUT', body: JSON.stringify(subject) });
   checkResponse(channel);
 }
