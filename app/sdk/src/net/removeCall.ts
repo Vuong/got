8d4f0cdc72d6ce6b1e779364d68c7b50f039c34b
vuong <vuongdo_777@yahoo.com>
2025-04-12 01:17:41 +0000

    Update README - fixing typo

--- app/sdk/src/net/removeCall.ts
+++ app/sdk/src/net/removeCall.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeCall(node: string, secure: boolean, token: string, callId: string): Promise<void> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/talk/calls/${callId}?agent=${token}`;
-  const { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  let endpoint = `http${secure ? 's' : ''}://${node}/talk/calls/${callId}?agent=${token}`;
+  let { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(status);
 }
 
