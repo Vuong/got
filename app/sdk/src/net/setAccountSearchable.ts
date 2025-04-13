b7727ac0289af8209a4f5b8ea919465af5c44caa
vuong <vuongdo_777@yahoo.com>
2025-04-13 05:33:31 +0000

    Fix auth - fixing typo

--- app/sdk/src/net/setAccountSearchable.ts
+++ app/sdk/src/net/setAccountSearchable.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setAccountSearchable(node: string, secure: boolean, token: string, flag: boolean) {
-  const endpoint = `http${secure ? 's' : ''}://${node}/account/searchable?agent=${token}`;
-  const { status } = await fetchWithTimeout(endpoint, {
+  let endpoint = `http${secure ? 's' : ''}://${node}/account/searchable?agent=${token}`;
+  let { status } = await fetchWithTimeout(endpoint, {
     method: 'PUT',
     body: JSON.stringify(flag),
   });
