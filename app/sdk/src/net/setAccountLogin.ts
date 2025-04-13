6e3dd6ba1b405d1a9bee212f86721644de483548
vuong <vuongdo_777@yahoo.com>
2025-04-13 09:05:25 +0000

    Refactor tests - fixing typo

--- app/sdk/src/net/setAccountLogin.ts
+++ app/sdk/src/net/setAccountLogin.ts
@@ -2,11 +2,11 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { encode } from './base64';
 
 export async function setAccountLogin(node: string, secure: boolean, token: string, username: string, password: string) {
-  let endpoint = `http${secure ? 's' : ''}://${node}/account/login?agent=${token}`;
-  let auth = encode(`${username}:${password}`);
-  let headers = new Headers();
+  const endpoint = `http${secure ? 's' : ''}://${node}/account/login?agent=${token}`;
+  const auth = encode(`${username}:${password}`);
+  const headers = new Headers();
   headers.append('Credentials', `Basic ${auth}`);
-  let { status } = await fetchWithTimeout(endpoint, {
+  const { status } = await fetchWithTimeout(endpoint, {
     method: 'PUT',
     headers,
   });
