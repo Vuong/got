65fd6906f6c98278b082ad6cc672bf9d3413f266
vuong <vuongdo_777@yahoo.com>
2025-04-14 06:31:00 +0000

    Remove UI - fixing typo

--- app/sdk/src/net/removeAccountMFAuth.ts
+++ app/sdk/src/net/removeAccountMFAuth.ts
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeAccountMFAuth(node: string, secure: boolean, token: string) {
-  let endpoint = `http${secure ? 's' : ''}://${node}/account/mfauth?agent=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
+  const endpoint = `http${secure ? 's' : ''}://${node}/account/mfauth?agent=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, { method: 'DELETE' });
   checkResponse(status);
 }
