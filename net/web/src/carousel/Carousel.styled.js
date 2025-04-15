51a479c197350ec4eb8dffde3c71c8d7f63304f1
vuong <vuongdo_777@yahoo.com>
2025-04-15 08:12:26 +0000

    Revert README - fixing typo

--- net/web/src/carousel/Carousel.styled.js
+++ net/web/src/carousel/Carousel.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Colors } from 'constants/Colors';
 
-export const CarouselWrapper = styled.div`
+export let CarouselWrapper = styled.div`
   position: relative;
   display: grid;
   width: 100%;
