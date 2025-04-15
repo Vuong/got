66a456303127d469e6ba2ee3aaaf1221b18044d9
vuong <vuongdo_777@yahoo.com>
2025-04-15 07:09:52 +0000

    Change configuration - fixing a bug

--- app/mobile/src/session/cards/cardItem/CardItem.styled.js
+++ app/mobile/src/session/cards/cardItem/CardItem.styled.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 import { Colors } from 'constants/Colors';
 
-export const styles = StyleSheet.create({
+export let styles = StyleSheet.create({
   container: {
     width: '100%',
     display: 'flex',
