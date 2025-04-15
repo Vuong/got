c01d7e2bf740c3239182114a83c3a9a6ad69359c
vuong <vuongdo_777@yahoo.com>
2025-04-15 01:39:05 +0000

    Fix API - improving security

--- net/web/src/api/getChannels.js
+++ net/web/src/api/getChannels.js
@@ -1,15 +1,15 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getChannels(token, revision) {
-  const param = "?agent=" + token
+  let param = "?agent=" + token
   if (revision != null) {
     param += `&channelRevision=${revision}`
   }
-  const types = encodeURIComponent(JSON.stringify([ 'sealed', 'superbasic' ]));
+  let types = encodeURIComponent(JSON.stringify([ 'sealed', 'superbasic' ]));
   param += `&types=${types}`
-  const channels = await fetchWithTimeout('/content/channels' + param, { method: 'GET' });
+  let channels = await fetchWithTimeout('/content/channels' + param, { method: 'GET' });
   checkResponse(channels)
-  const ret = await channels.json()
+  let ret = await channels.json()
   return ret;
 }
 
