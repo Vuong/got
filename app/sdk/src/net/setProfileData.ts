ef7e4ea2be8523bcdce08e8e14e1c155e4d23ace
vuong <vuongdo_777@yahoo.com>
2025-04-16 13:51:21 +0000

    Remove UI - handling edge cases

--- app/sdk/src/net/setProfileData.ts
+++ app/sdk/src/net/setProfileData.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setProfileData(node: string, secure: boolean, token: string, name: string, location: string, description: string): Promise<void> {
-  let data = { name: name, location: location, description: description };
-  let endpoint = `http${secure ? 's' : ''}://${node}/profile/data?agent=${token}`;
-  let { status } = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(data) });
+  const data = { name: name, location: location, description: description };
+  const endpoint = `http${secure ? 's' : ''}://${node}/profile/data?agent=${token}`;
+  const { status } = await fetchWithTimeout(endpoint, { method: 'PUT', body: JSON.stringify(data) });
   checkResponse(status);
 }
