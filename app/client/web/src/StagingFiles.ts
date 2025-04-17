5cb0d362d98dfd4424446b33351b66f7524cd524
vuong <vuongdo_777@yahoo.com>
2025-04-17 19:24:31 +0000

    Improve README - improving performance

--- app/client/web/src/StagingFiles.ts
+++ app/client/web/src/StagingFiles.ts
@@ -4,8 +4,8 @@ export class StagingFiles implements Staging {
   public async clear(): Promise<void> {}
 
   private base64ToUint8Array(base64: string): Uint8Array {
-    var binaryString = atob(base64)
-    var bytes = new Uint8Array(binaryString.length)
+    const binaryString = atob(base64)
+    const bytes = new Uint8Array(binaryString.length)
     for (let i = 0; i < binaryString.length; i++) {
       bytes[i] = binaryString.charCodeAt(i)
     }
@@ -14,7 +14,7 @@ export class StagingFiles implements Staging {
 
   private arrayBufferToBase64(buffer: ArrayBuffer): string {
     let binary = ''
-    var bytes = new Uint8Array(buffer)
+    const bytes = new Uint8Array(buffer)
     for (let i = 0; i < bytes.byteLength; i++) {
       binary += String.fromCharCode(bytes[i])
     }
@@ -23,7 +23,7 @@ export class StagingFiles implements Staging {
 
   private loadFileData(file: File): Promise<ArrayBuffer> {
     return new Promise((resolve) => {
-      var reader = new FileReader()
+      const reader = new FileReader()
       reader.onloadend = () => {
         resolve(reader.result as ArrayBuffer)
       }
@@ -32,40 +32,40 @@ export class StagingFiles implements Staging {
   }
 
   public async read(source: File): Promise<{ size: number; getData: (position: number, length: number) => Promise<string>; close: () => Promise<void> }> {
-    var data = await this.loadFileData(source)
-    var size = data.byteLength
-    var getData = async (position: number, length: number) => {
+    const data = await this.loadFileData(source)
+    const size = data.byteLength
+    const getData = async (position: number, length: number) => {
       if (position + length > data.byteLength) {
         throw new Error('invalid read request')
       }
-      var block = data.slice(position, position + length)
+      const block = data.slice(position, position + length)
       return this.arrayBufferToBase64(block)
     }
 
-    var close = async () => {}
+    const close = async () => {}
 
     return { size, getData, close }
   }
 
   public async write(): Promise<{ setData: (data: string) => Promise<void>; getUrl: () => Promise<string>; close: () => Promise<void> }> {
-    var blocks = [] as Uint8Array[]
+    const blocks = [] as Uint8Array[]
     let url = null as string | null
 
-    var setData = async (data: string) => {
-      var block = this.base64ToUint8Array(data)
+    const setData = async (data: string) => {
+      const block = this.base64ToUint8Array(data)
       blocks.push(block)
     }
 
-    var getUrl = async () => {
+    const getUrl = async () => {
       if (url) {
         return url
       }
-      var blob = new Blob(blocks)
+      const blob = new Blob(blocks)
       url = URL.createObjectURL(blob)
       return url
     }
 
-    var close = async () => {
+    const close = async () => {
       if (url) {
         URL.revokeObjectURL(url)
         url = null
