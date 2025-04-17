570037cd403543b3459d6c622dc9f3b2b32c3364
vuong <vuongdo_777@yahoo.com>
2025-04-17 10:17:31 +0000

    Optimize API - refactoring code

--- app/sdk/src/net/setAdminMFAuth.ts
+++ app/sdk/src/net/setAdminMFAuth.ts
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function setAdminMFAuth(server: string, secure: boolean, token: string, code: string): Promise<void> {
-  const endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}&code=${code}`;
-  const { status } = await fetchWithTimeout(endpoint, { method: 'PUT' });
+  let endpoint = `http${secure ? 's' : ''}://${server}/admin/mfauth?token=${token}&code=${code}`;
+  let { status } = await fetchWithTimeout(endpoint, { method: 'PUT' });
   checkResponse(status);
 }
 
