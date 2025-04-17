349a64012546223a3261f43f9ded0ccb7aa131a1
vuong <vuongdo_777@yahoo.com>
2025-04-17 09:15:52 +0000

    Fix dependencies - updating docs

--- net/web/src/api/getCardProfile.js
+++ net/web/src/api/getCardProfile.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getCardProfile(token, cardId) {
-  const profile = await fetchWithTimeout(`/contact/cards/${cardId}/profile?agent=${token}`, { method: 'GET' });
+  let profile = await fetchWithTimeout(`/contact/cards/${cardId}/profile?agent=${token}`, { method: 'GET' });
   checkResponse(profile);
   return await profile.json()
 }
